abca99d4b49ee82992252bff956684906525be84
David Stone <davidjamesstone@gmail.com>
2018-09-20 12:02:36 +0100

    Browser refresh init

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const createServer = require('./server')
 
 createServer()
   .then(server => server.start())
+  .then(() => process.send && process.send('online'))
   .catch(err => {
     console.log(err)
     process.exit(1)
