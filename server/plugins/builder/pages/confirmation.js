1c523cc39ec86ab26e15939441df04ec77bf772e
Jen <jen+git@cautionyourblast.com>
2019-10-18 18:09:42 +0100

    thailand endpoint, confirmation pages

---
+++ server/plugins/builder/pages/confirmation.js
@@ -0,0 +1,21 @@
+const Page = require('.')
+const { payStatus } = require('../../pay')
+
+class ConfirmationPage extends Page {
+  makeGetRouteHandler (getState) {
+    return async (request, h) => {
+      const model = this.model
+      //const { self } = request.yar.get('pay')
+      // try {
+      //   let res = await payStatus(self)
+      //   console.log(res)
+      // } catch (ex) {
+      //   // error with payRequest
+      //   console.log(ex)
+      // }
+
+      return h.view('confirmation', model)
+    }
+  }
+}
+module.exports = ConfirmationPage
