0c4ec3db2f18360161e51de55c15598fdd6746cd
Jen <jen+git@cautionyourblast.com>
2019-10-29 15:31:52 +0000

    ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,18 @@
 module.exports = {
   apps: [{
-    name: 'app',
+    name: 'app_designer',
+    script: 'index.js',
+    instances: 1,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  },
+  {
+    name: 'app_runner',
     script: 'index.js',
     instances: 1,
     autorestart: true,
