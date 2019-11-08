53ca51339d0c018b0d72558d00d01a305e50ca19
Max Stewart <max@cautionyourblast.com>
2019-11-08 15:20:56 +0000

    Increase timeout for CM submission.

--- server/lib/caseManagement.js
+++ server/lib/caseManagement.js
@@ -4,14 +4,18 @@ const { caseManagementApiUrl } = require('./../config')
 const options = {
   headers: {
     'content-type': 'application/json'
-  }
+  },
+  timeout: 60000
 }
 
 const caseManagementPostRequest = async (answers) => {
   const data = { ...options, payload: JSON.stringify(answers) }
   try {
     const { payload } = await Wreck.post(`${caseManagementApiUrl}/v1/applications`, data)
-    return JSON.parse(payload.toString())
+    if (payload && payload.toString()) {
+      return JSON.parse(payload.toString())
+    }
+    return { reference: 'unknown' }
   } catch (e) {
     throw e
   }
