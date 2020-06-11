fefba88324bc30f8a8ecc5bbf5647fc4fa1df060
Andy Moody <moody.andy@gmail.com>
2020-06-11 14:35:18 +0100

    Inline conditions react component update
    * Uses the new inline conditions model
    * Now capable of supporting and / or conditions
    * Adds conditions individually, presenting the overall condition
      as a human readable string back to the user.
    * Applies default condition precedence rules with no ability to
      override

--- designer/client/inline-conditions.js
+++ designer/client/inline-conditions.js
@@ -1,75 +1,18 @@
 import React from 'react'
-import componentTypes from 'digital-form-builder-engine/component-types'
+import { ConditionsModel, Condition, Field, Value } from './inline-condition-model'
+import { clone } from './helpers'
 
 const conditionalOperators = {
   'default': {
-    is : {},
+    is: {},
     'is not': {}
   },
   'TextField': {
     matches: {}
   },
 
-  getConditionals: (fieldType) =>  {
+  getConditionals: (fieldType) => {
     return Object.assign(conditionalOperators[fieldType] || {}, conditionalOperators['default'])
-  }
-}
-
-class ConditionsModel {
-  constructor () {
-    this.conditions = {}
-  }
-
-  uniqueId() {
-    var id = Date.now();
-    if(this.conditions[id]) {
-      return this.uniqueId();
-    }
-    return id;
-  }
-
-  and() {
-    const id = this.uniqueId();
-    this.conditions[id] = {id: id};
-    return this;
-  }
-
-  setField(id, fieldName) {
-    this._getCondition(id).field = fieldName;
-    return this;
-  }
-
-  getField(id) {
-    return this._getCondition(id).field;
-  }
-
-  setOperator(id, operator) {
-    this._getCondition(id).operator = operator;
-    return this;
-  }
-
-  getOperator(id) {
-    return this._getCondition(id).operator;
-  }
-
-  setValue(id, value) {
-    this._getCondition(id).value = value;
-    return this;
-  }
-
-  getValue(id) {
-    return this._getCondition(id).value;
-  }
-
-  asArray() {
-   return Object.values(this.conditions)
-  }
-
-  _getCondition(id) {
-    if (!this.conditions[id]) {
-      throw Error("no such condition "+id)
-    }
-    return this.conditions[id];
   }
 }
 
@@ -78,13 +21,14 @@ class InlineConditions extends React.Component {
     super(props)
 
     const { data } = this.props
+
     this.fields = [].concat.apply([],
       data.pages.map(page => page.components.filter(component => component.name)
-        .map(component => ({label: component.title, name: component.name, type: component.type, values: component.options.list ? data.lists.find(it => it.name === component.options.list).items : []})))
+        .map(component => ({ label: component.title, name: component.name, type: component.type, values: component.options.list ? data.lists.find(it => it.name === component.options.list).items : [] })))
     ).reduce((obj, item) => {
       obj[item.name] = item
       return obj
-    }, {});
+    }, {})
 
     this.state = {
       conditions: new ConditionsModel()
@@ -93,108 +37,164 @@ class InlineConditions extends React.Component {
 
   onClickAddItem = e => {
     this.setState({
-      conditions: this.state.conditions.and()
+      condition: {}
     })
   }
 
-  conditionFieldId(id, fieldName) {
+  onClickAnd = e => {
+    this.setState({
+      condition: {
+        coordinator: 'and'
+      }
+    })
+  }
+
+  onClickOr = e => {
+    this.setState({
+      condition: {
+        coordinator: 'or'
+      }
+    })
+  }
+
+  onClickSave = e => {
+    const { conditions, condition } = this.state
+
+    this.setState({
+      conditions: conditions.add(new Condition(Field.from(condition.field), condition.operator, Value.from(condition.value), condition.coordinator)),
+      condition: undefined
+    })
+  }
+
+  conditionFieldId (id, fieldName) {
     return `conditions.${id}.${fieldName}`
   }
 
-  parseConditionId(fieldId) {
+  parseConditionId (fieldId) {
     return fieldId.split('.')[1]
   }
 
   onChangeField = e => {
     const input = e.target
-    const conditionIdentifier = this.parseConditionId(input.name)
-    const fieldName = input.value;
+    const fieldName = input.value
 
-    const currentField = this.state.conditions.getField(conditionIdentifier);
+    const { condition } = this.state
 
-    if (currentField && this.fields[currentField].values !== this.fields[fieldName].values) {
-      this.state.conditions.setValue(conditionIdentifier, undefined)
-    }
+    const currentField = condition.field ? condition.field.name : undefined
+    const currentOperator = condition.operator
 
-    const currentOperator = this.state.conditions.getOperator(conditionIdentifier);
-    if(currentOperator && !conditionalOperators.getConditionals(fieldName)[currentOperator]) {
-      this.state.conditions.setOperator(conditionIdentifier, undefined);
-    }
+    this._updateCondition(condition, c => {
+      if (fieldName) {
+        if (currentField && this.fields[currentField].values !== this.fields[fieldName].values) {
+          delete c.value
+        }
+        if (currentOperator && !conditionalOperators.getConditionals(fieldName)[currentOperator]) {
+          delete c.operator
+        }
+        c.field = new Field(fieldName, this.fields[fieldName].label)
+      } else {
+        delete c.field
+        delete c.operator
+        delete c.value
+      }
+    })
+  }
 
+  _updateCondition (condition, updates) {
+    const copy = clone(condition)
+    updates(copy)
     this.setState({
-      conditions: this.state.conditions.setField(conditionIdentifier, fieldName)
+      condition: copy
     })
   }
 
   onChangeOperator = e => {
     const input = e.target
-    const conditionIdentifier = this.parseConditionId(input.name)
-    this.setState({
-      conditions: this.state.conditions.setOperator(conditionIdentifier, input.value)
-    })
+    const { condition } = this.state
+
+    this._updateCondition(condition, c => { c.operator = input.value })
   }
 
   onChangeValue = e => {
     const input = e.target
-    const conditionIdentifier = this.parseConditionId(input.name)
-    this.setState({
-      conditions: this.state.conditions.setValue(conditionIdentifier, input.value)
-    })
+    const { condition } = this.state
+
+    this._updateCondition(condition, c => { c.value = new Value(input.value) })
   }
 
   render () {
-    const conditions = this.state.conditions.asArray();
+    const { conditions, condition } = this.state
+
+    const fieldDef = condition && condition.field ? this.fields[condition.field.name] : undefined
+
+    const hasConditions = conditions.hasConditions()
 
     return (
       <div>
-      { conditions.map((condition, index) => (
-          <div key={condition.id} >
-            <div className='govuk-form-group'>
-              <label className='govuk-label govuk-label--s' htmlFor='field'>{index == 0 ? 'When' : 'And'}</label>
-              <select className='govuk-select' id={this.conditionFieldId(condition.id, 'field')} name={this.conditionFieldId(condition.id, 'field')} value={condition.field}
-                onChange={this.onChangeField}>
-                <option />
-                {Object.values(this.fields).map(field => {
-                  return <option key={field.name} value={field.name}>{field.label}</option>
-                })}
-              </select>
-
-              { condition.field &&
-                <select className='govuk-select' id={this.conditionFieldId(condition.id, 'operator')} name={this.conditionFieldId(condition.id, 'operator')} value={condition.operator}
-                  onChange={this.onChangeOperator}>
-                  <option />
-                  {
-                    Object.keys(conditionalOperators.getConditionals(this.fields[condition.field].type)).map(conditional => {
-                      return <option key={condition.field + '-' + conditional} value={conditional}>{conditional}</option>
-                    })
-                  }
-                </select>
-              }
-
-              { condition.operator && this.fields[condition.field].values && this.fields[condition.field].values.length > 0 &&
-                <select className='govuk-select' id={this.conditionFieldId(condition.id, 'value')} name={this.conditionFieldId(condition.id, 'value')} value={condition.value}
-                  onChange={this.onChangeValue}>
-                  <option />
-                  {this.fields[condition.field].values.map(option => {
-                    return <option key={option.value} value={option.value}>{option.text}</option>
-                  })}
-                </select>
-              }
-
-              { condition.operator && (!this.fields[condition.field].values || this.fields[condition.field].values.length === 0) &&
-                <input className='govuk-input govuk-input--width-20' id={this.conditionFieldId(condition.id, 'value')} name={this.conditionFieldId(condition.id, 'value')}
-                  type='text' defaultValue={condition.value} required
-                  onBlur={this.onChangeValue} />
-              }
+        { hasConditions &&
+          <div className='govuk-form-group'>
+            <label className='govuk-label govuk-label--s' htmlFor='condition-string'>When</label>
+            <div key='condition-string' >
+              {conditions.toPresentationString()}
             </div>
           </div>
-        ))
-       }
-       <a href='#' onClick={this.onClickAddItem}>Add</a>
-       </div>
+        }
+        { condition &&
+        <div className='govuk-form-group'>
+          <label className='govuk-label govuk-label--s' htmlFor='field'>{hasConditions ? condition.coordinator : 'When'}</label>
+          <select className='govuk-select' id='cond-field' name='cond-field' value={condition && condition.field ? condition.field.name : undefined}
+            onChange={this.onChangeField}>
+            <option />
+            {Object.values(this.fields).map(field => {
+              return <option key={field.name} value={field.name}>{field.label}</option>
+            })}
+          </select>
+
+          { fieldDef &&
+          <select className='govuk-select' id='cond-operator' name='cond-operator' value={condition.operator || undefined}
+            onChange={this.onChangeOperator}>
+            <option />
+            {
+              Object.keys(conditionalOperators.getConditionals(fieldDef.type)).map(conditional => {
+                return <option key={condition.field + '-' + conditional} value={conditional}>{conditional}</option>
+              })
+            }
+          </select>
+          }
+
+          { condition.operator && fieldDef.values && fieldDef.values.length > 0 &&
+          <select className='govuk-select' id='cond-value' name='cond-value' value={condition.value ? condition.value.value : undefined}
+            onChange={this.onChangeValue}>
+            <option />
+            {fieldDef.values.map(option => {
+              return <option key={option.value} value={option.value}>{option.text}</option>
+            })}
+          </select>
+          }
+
+          { condition.operator && (!fieldDef.values || fieldDef.values.length === 0) &&
+          <input className='govuk-input govuk-input--width-20' id='cond-value' name='cond-value'
+            type='text' defaultValue={condition.value} required
+            onChange={this.onChangeValue} />
+          }
+        </div>
+        }
+        { condition && condition.value &&
+          <div className='govuk-form-group'>
+            <a href='#' onClick={this.onClickSave}>Save condition</a>
+          </div>
+        }
+        {!condition && !hasConditions &&
+          <a href='#' onClick={this.onClickAddItem}>Add</a>
+        }
+        {!condition && hasConditions &&
+          <div className='govuk-form-group'>
+            <a href='#' onClick={this.onClickAnd}>And</a> / <a href='#' onClick={this.onClickOr}>Or</a>
+          </div>
+        }
+      </div>
     )
   }
-
 }
 
 export default InlineConditions
