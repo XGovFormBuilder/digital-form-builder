d69bae10e8239830b88f0c92af671de85008b894
Andy Moody <moody.andy@gmail.com>
2020-06-17 16:05:56 +0100

    Remove dead file

--- designer/test/helper.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const chai = require('chai')
-
-global.should = chai.should()
-global.expect = chai.expect
-global.sinon = require('sinon')
-chai.use(require('sinon-chai'))
-chai.use(require('chai-as-promised'))
