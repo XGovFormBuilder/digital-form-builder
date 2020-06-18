6ef6259cbf015f6368ffca687eab304a570ed7b1
Andy Moody <moody.andy@gmail.com>
2020-06-18 12:20:14 +0100

    Edit and move icons and styling

--- designer/client/inline-conditions.js
+++ designer/client/inline-conditions.js
@@ -308,23 +308,37 @@ class InlineConditions extends React.Component {
             <div className='govuk-checkboxes'>
               {
                 this.state.conditions.asPerUserGroupings().map((condition, index) => {
-                  return <div key={`condition-checkbox-${index}`} className='govuk-checkboxes__item'>
+                  return <div key={`condition-checkbox-${index}`} className='govuk-checkboxes__item' style={{ display: 'flex' }}>
                     <input type='checkbox' className='govuk-checkboxes__input' id={`condition-${index}`} name={`condition-${index}`}
                       value={index} onChange={this.onChangeCheckbox} checked={(this.state.selectedConditions??[]).includes(index) || ''} />
                     <label className='govuk-label govuk-checkboxes__label' htmlFor={`condition-${index}`}>
                       {condition.toPresentationString()}
                     </label>
-                    <span id={`condition-${index}-actions`}>
-                      {condition.isGroup() && <span>  <a href='#' id={`condition-${index}-split`} onClick={() => this.onClickSplit(index)}>Split</a></span>}
-                      {!condition.isGroup() && <span>  <a href='#' id={`condition-${index}-edit`} onClick={() => this.onClickEdit(index)}>Edit</a></span>}
+                    <span id={`condition-${index}-actions`} style={{ display: 'inline-flex', flexGrow: 1 }}>
+                      {condition.isGroup() && <span style={{ flexGrow: 1 }}>  <a href='#' id={`condition-${index}-split`} onClick={() => this.onClickSplit(index)}>Split</a></span>}
+                      {!condition.isGroup() && <span style={{ flexGrow: 1 }}>  <a href='#' id={`condition-${index}-edit`} onClick={() => this.onClickEdit(index)}>
+                        <svg height='0.6em' width='1em' transform='scale(1.8)' style={{ verticalAlign: 'bottom' }}>
+                          <polyline points='1.5,9 9,1' style={{ fill: 'none', stroke: 'grey', strokeWidth: '2', strokeLinecap: 'butt' }} />
+                          <polyline points='9,1 9,1' style={{ fill: 'none', stroke: 'grey', strokeWidth: '2', strokeLinecap: 'round', strokeLinejoin: 'round' }} />
+                          <line x1='1' x2='8.5' y1='9' y2='1' style={{ stroke: 'gainsboro', strokeWidth: '0.1' }} />
+                          <line x1='1.5' x2='9' y1='9.5' y2='1.5' style={{ stroke: 'gainsboro', strokeWidth: '0.1' }} />
+                          <line x1='8' x2='9.5' y1='0.6' y2='2' style={{ stroke: 'gainsboro', strokeWidth: '0.7' }} />
+                          <ellipse cx='1.5' cy='9' rx='1' ry='0.5' transform='rotate(45, 1.5, 9)' style={{ fill: 'gainsboro' }} />
+                          <polygon points='0,10.5 0.8,8.3 2.2,9.7' style={{ fill: 'gainsboro' }} />
+                          <polygon points='0,10.5 0.3,9.7 0.9,10.2' style={{ fill: 'grey' }} />
+                              Edit
+                        </svg>
+                      </a>
+                      </span>
+                      }
                       {index > 0 && <span>  <a href='#' id={`condition-${index}-move-earlier`} onClick={() => this.onClickMoveEarlier(index)}>
-                        <svg height='10' width='20'>
+                        <svg height='0.6em' width='1.5em'>
                           <polyline points='1,9 10,2 19,9' style={{ fill: 'none', stroke: 'grey', strokeWidth: 4, strokeLinecap: 'round', strokeLinejoin: 'round' }} />
                           Move up
                         </svg>
                       </a></span>}
                       {index < this.state.conditions.lastIndex() && <span>  <a href='#' id={`condition-${index}-move-later`} onClick={() => this.onClickMoveLater(index)}>
-                        <svg height='10' width='20'>
+                        <svg height='0.6em' width='1.5em'>
                           <polyline points='1,1 10,9 19,1' style={{ fill: 'none', stroke: 'grey', strokeWidth: 4, strokeLinecap: 'round', strokeLinejoin: 'round' }} />
                             Move down
                         </svg>
