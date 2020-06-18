7a61e488a33c5f34da4ed201caae05e0e54f8096
Andy Moody <moody.andy@gmail.com>
2020-06-18 15:19:29 +0100

    Move icons to their own file

--- designer/client/inline-conditions.js
+++ designer/client/inline-conditions.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { ConditionsModel, Condition, Field, Value, GroupDef } from './inline-condition-model'
 import { clone } from './helpers'
+import { icons } from './icons'
 
 const conditionalOperators = {
   'default': {
@@ -324,31 +325,15 @@ class InlineConditions extends React.Component {
                     <span id={`condition-${index}-actions`} style={{ display: 'inline-flex', flexGrow: 1 }}>
                       {condition.isGroup() && <span style={{ flexGrow: 1 }}>  <a href='#' id={`condition-${index}-split`} onClick={() => this.onClickSplit(index)}>Split</a></span>}
                       {!condition.isGroup() && <span style={{ flexGrow: 1 }}>  <a href='#' id={`condition-${index}-edit`} onClick={() => this.onClickEdit(index)}>
-                        <svg height='0.6em' width='1em' transform='scale(1.8)' style={{ verticalAlign: 'bottom' }}>
-                          <polyline points='1.5,9 9,1' style={{ fill: 'none', stroke: 'grey', strokeWidth: '2', strokeLinecap: 'butt' }} />
-                          <polyline points='9,1 9,1' style={{ fill: 'none', stroke: 'grey', strokeWidth: '2', strokeLinecap: 'round', strokeLinejoin: 'round' }} />
-                          <line x1='1' x2='8.5' y1='9' y2='1' style={{ stroke: 'gainsboro', strokeWidth: '0.1' }} />
-                          <line x1='1.5' x2='9' y1='9.5' y2='1.5' style={{ stroke: 'gainsboro', strokeWidth: '0.1' }} />
-                          <line x1='8' x2='9.5' y1='0.6' y2='2' style={{ stroke: 'gainsboro', strokeWidth: '0.7' }} />
-                          <ellipse cx='1.5' cy='9' rx='1' ry='0.5' transform='rotate(45, 1.5, 9)' style={{ fill: 'gainsboro' }} />
-                          <polygon points='0,10.5 0.8,8.3 2.2,9.7' style={{ fill: 'gainsboro' }} />
-                          <polygon points='0,10.5 0.3,9.7 0.9,10.2' style={{ fill: 'grey' }} />
-                              Edit
-                        </svg>
+                        {icons.edit}
                       </a>
                       </span>
                       }
                       {index > 0 && <span>  <a href='#' id={`condition-${index}-move-earlier`} onClick={() => this.onClickMoveEarlier(index)}>
-                        <svg height='0.6em' width='1.5em'>
-                          <polyline points='1,9 10,2 19,9' style={{ fill: 'none', stroke: 'grey', strokeWidth: 4, strokeLinecap: 'round', strokeLinejoin: 'round' }} />
-                          Move up
-                        </svg>
+                        {icons.moveUp}
                       </a></span>}
                       {index < this.state.conditions.lastIndex() && <span>  <a href='#' id={`condition-${index}-move-later`} onClick={() => this.onClickMoveLater(index)}>
-                        <svg height='0.6em' width='1.5em'>
-                          <polyline points='1,1 10,9 19,1' style={{ fill: 'none', stroke: 'grey', strokeWidth: 4, strokeLinecap: 'round', strokeLinejoin: 'round' }} />
-                            Move down
-                        </svg>
+                        {icons.moveDown}
                       </a></span>}
                     </span>
                   </div>
