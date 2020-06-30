0de46141ab38e1e9f75e8c40bec58233c2ea9f6d
Max Stewart <max@cautionyourblast.com>
2020-06-30 13:24:38 +0100

    Use correct pageId when filtering out irrelevant detail items.

--- runner/src/server/plugins/builder/pages/summary.js
+++ runner/src/server/plugins/builder/pages/summary.js
@@ -263,7 +263,7 @@ class SummaryViewModel {
       }
 
       for (let index = 0; index < items.length; index++) {
-        const item = items[index].filter(detailItem => detailItem.pageId === page.path)
+        const item = items[index].filter(detailItem => detailItem.pageId === `/${model.basePath}${page.path}`)
         const fields = []
 
         for (const detailItem of item) {
