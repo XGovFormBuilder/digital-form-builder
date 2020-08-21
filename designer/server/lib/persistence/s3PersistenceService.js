e0f4e84c6f8858b11561a5a0d340badf254e64d4
Andy Moody <moody.andy@gmail.com>
2020-08-21 14:34:19 +0100

    Added tests for s3PersistenceService.js

--- designer/server/lib/persistence/s3PersistenceService.js
+++ designer/server/lib/persistence/s3PersistenceService.js
@@ -23,11 +23,11 @@ export class S3PersistenceService implements PersistenceService {
       this.logger.error(`error listing all configurations ${response.error.message}`)
       return response.error
     }
-    return response.Contents.map(entry => new FormConfiguration(entry.Key.replace('.json', ''), entry.Metadata["x-amz-meta-name"], entry.LastModified))
+    return response.Contents.map(entry => new FormConfiguration(entry.Key.replace('.json', ''), (entry.Metadata??{})['x-amz-meta-name'], entry.LastModified))
   }
 
   async getConfiguration (id: string) {
-    const response = await this.bucket.getObject({ Key: `${this.ensureJsonExtension(id)}` }).promise()
+    const response = await this.bucket.getObject({ Key: `${this._ensureJsonExtension(id)}` }).promise()
     if (response.error) {
       this.logger.error(`error getting configuration with id: ${id}, ${response.error.message}`)
       return response.error
@@ -37,8 +37,8 @@ export class S3PersistenceService implements PersistenceService {
   }
 
   async uploadConfiguration (id: string, configuration: any) {
-    id = this.ensureJsonExtension(id)
-    const metadata = {"x-amz-meta-name": JSON.parse(configuration).name}
+    id = this._ensureJsonExtension(id)
+    const metadata = { 'x-amz-meta-name': JSON.parse(configuration).name }
     const response = await this.bucket.upload({ Key: id, Body: configuration, Metadata: metadata }).promise()
     if (response.error) {
       this.logger.error(`error uploading configuration with id: ${id} ${response.error.message}`)
@@ -47,10 +47,10 @@ export class S3PersistenceService implements PersistenceService {
   }
 
   async copyConfiguration (configurationId: string, newName: string) {
-    configurationId = this.ensureJsonExtension(configurationId)
+    configurationId = this._ensureJsonExtension(configurationId)
     const response = await this.bucket.copyObject({
       CopySource: encodeURI(`${this.bucket.config.params.Bucket}/${configurationId}`),
-      Key: `${newName}.json`
+      Key: this._ensureJsonExtension(newName)
     }).promise()
     if (response.error) {
       this.logger.error(`error copying configuration with id: ${configurationId}, with new name ${newName}, ${response.error.message}`)
@@ -58,10 +58,10 @@ export class S3PersistenceService implements PersistenceService {
     return response
   }
 
-  ensureJsonExtension(configurationId) {
+  _ensureJsonExtension (configurationId: string) {
     if (!configurationId.endsWith('.json')) {
       configurationId = `${configurationId}.json`
     }
-    return configurationId;
+    return configurationId
   }
 }
