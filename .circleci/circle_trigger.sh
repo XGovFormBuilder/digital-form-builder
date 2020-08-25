f187b2365e0d2fbd06c7e94692dd586afebe7d79
Jen <jen+git@cautionyourblast.com>
2020-08-25 11:25:26 +0100

    remove comment and other test files

--- .circleci/circle_trigger.sh
+++ .circleci/circle_trigger.sh
@@ -107,9 +107,6 @@ fi
 
 echo "Changes detected in ${COUNT} package(s)."
 
-#PARAMETERS='"trigger":false, "designer":true, "runner":true, "engine":true'
-
-
 ############################################
 ## 3. CicleCI REST API call
 ############################################
