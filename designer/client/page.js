91887f0596fa0b770fa6ebfc524535f651ed9e64
bertrandmc <bemacode@gmail.com>
2020-09-15 17:25:50 +0100

    Change copy and make Preview button visible

--- designer/client/page.js
+++ designer/client/page.js
@@ -24,7 +24,10 @@ const SortableList = SortableContainer(({ page, data }) => {
 })
 
 class Page extends React.Component {
-  state = {}
+  state = {
+    showEditor: false,
+    showAddComponent: false
+  }
 
   showEditor = (e, value) => {
     e.stopPropagation()
@@ -47,13 +50,32 @@ class Page extends React.Component {
     // data.save(data)
   }
 
+  toggleAddComponent = () => {
+    this.setState(prevState => ({ 
+      showAddComponent: !prevState.showAddComponent
+    }));
+  }
+
+  toggleEditor = () => {
+    this.setState(prevState => ({ 
+      showEditor: !prevState.showEditor 
+    }));
+  }
+
   render () {
     const { page, data, id, previewUrl, persona } = this.props
     const { sections } = data
-    const formComponents = page.components.filter(comp => ComponentTypes.find(type => type.name === comp.type).subType === 'field')
+    const formComponents = page?.components?.filter(comp => 
+      ComponentTypes.find(type => type.name === comp.type).subType === 'field'
+    )
     const section = page.section && sections.find(section => section.name === page.section)
     const conditional = !!page.condition
-    let pageTitle = page.title || (formComponents.length === 1 && page.components[0] === formComponents[0] ? formComponents[0].title : page.title)
+    let pageTitle =
+      page.title ||
+      (formComponents.length === 1 && page.components[0] === formComponents[0]
+        ? formComponents[0].title
+        : page.title);
+    
     if (pageTitle && typeof pageTitle === 'object') {
       pageTitle = pageTitle.en
     }
@@ -64,10 +86,11 @@ class Page extends React.Component {
         id={page.path} className={`page${conditional ? ' conditional' : ''} ${highlight ? 'highlight' : ''}`}
         title={page.path} style={this.props.layout}
       >
-        <div className='handle' onClick={(e) => this.showEditor(e, true)} />
-        <div className='govuk-!-padding-top-2 govuk-!-padding-left-2 govuk-!-padding-right-2'>
-          <h3 className='govuk-heading-s'>
-            {section && <span className='govuk-caption-m govuk-!-font-size-14'>{section.title}</span>}
+        <div className='page__handle' onClick={this.toggleEditor} />
+        
+        <div className='page__heading'>
+          <h3>
+            {section && <span>{section.title}</span>}
             {pageTitle}
           </h3>
         </div>
@@ -78,35 +101,37 @@ class Page extends React.Component {
           lockToContainerEdges useDragHandle
         />
 
-        <div className='govuk-!-padding-2'>
-          <a
-            className='preview pull-right govuk-body govuk-!-font-size-14'
-            href={`${previewUrl}/${id}${page.path}`} target='_blank'
-          >Open
-          </a>
-          <div
-            className='button active'
-            onClick={e => this.setState({ showAddComponent: true })}
+        <div className='page__actions'>
+          <button
+            className='page__actions__button active'
+            onClick={this.toggleAddComponent}
           />
+          <a
+            title="Preview page"
+            className='page__actions__preview'
+            href={`${previewUrl}/${id}${page.path}`} target='_blank'
+          >Preview</a>
         </div>
 
         <Flyout
           title='Edit Page' show={this.state.showEditor}
-          onHide={e => this.showEditor(e, false)}
+          onHide={this.toggleEditor}
         >
           <PageEdit
             page={page} data={data}
-            onEdit={e => this.setState({ showEditor: false })}
+            onEdit={this.toggleEditor}
           />
         </Flyout>
 
         <Flyout
-          title='Add Component' show={this.state.showAddComponent}
-          onHide={() => this.setState({ showAddComponent: false })}
+          title='Add Component' 
+          show={this.state.showAddComponent}
+          onHide={this.toggleAddComponent}
         >
-          <ComponentCreate
-            page={page} data={data}
-            onCreate={e => this.setState({ showAddComponent: false })}
+          <ComponentCreate 
+            page={page} 
+            data={data} 
+            onCreate={this.toggleAddComponent} 
           />
         </Flyout>
       </div>
