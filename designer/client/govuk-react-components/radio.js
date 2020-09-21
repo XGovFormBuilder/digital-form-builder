1ad0151756522da40f3d93a4c99ea2aa14f56a01
bertrandmc <bemacode@gmail.com>
2020-09-21 09:54:34 +0100

    Refactor Radio Component

--- designer/client/govuk-react-components/radio.js
+++ designer/client/govuk-react-components/radio.js
@@ -1,48 +1,64 @@
 // @flow
 import React from 'react'
-import { InputOptions, renderHints } from './helpers'
+import { renderHints } from './helpers'
 
-export function radioGroup (name: string, heading: string, items: Array<RadioOption>, options: ?InputOptions) {
-  return <fieldset className='govuk-fieldset' aria-describedby={`${name}-hint`}>
-    <legend className='govuk-fieldset__legend govuk-fieldset__legend--l'>
-      <h1 className='govuk-fieldset__heading'>
-        {heading}
-      </h1>
-    </legend>
-    { renderHints(name, options?.hints) }
+export class RadioGroup extends React.Component {
+  radioId = Date.now()
 
-    <div className='govuk-radios govuk-radios--inline'>
-      {items.map((option, index) =>
-        <div key={`${option.id}-${index}`} className='govuk-radios__item'>
-          <input className='govuk-radios__input' id={option.id} name={name} type='radio' value={option.value} defaultChecked={option.checked} onClick={option.onClick} aria-describedby={`${option.id}-hint`} required={options?.required}/>
-          <label className='govuk-label govuk-radios__label' htmlFor={option.id}>
-            {option.text}
-          </label>
-          {option.hint &&
-            <div id={`${option.id}-hint`} className="govuk-hint govuk-radios__hint">
-              {option.hint}
-            </div>
-          }
+  render () {
+    const { name, heading, children, options, value, onClick } = this.props
+
+    return (
+      <fieldset className='govuk-fieldset' aria-describedby={`${name}-hint`}>
+        <legend className='govuk-fieldset__legend govuk-fieldset__legend--l'>
+          <h1 className='govuk-fieldset__heading'>
+            {heading}
+          </h1>
+        </legend>
+
+        { renderHints(name, options?.hints) }
+
+        <div className='govuk-radios govuk-radios--inline'>
+          {children.map(child => (
+            React.cloneElement(child, {
+              name,
+              id: `radio-${this.radioId}`,
+              key: `key-${child.props.value}`,
+              required: options?.required,
+              defaultChecked: value === child.props.value,
+              onClick
+            })
+          ))}
+        </div>
+      </fieldset>
+    )
+  }
+}
+
+export function RadioOption (props) {
+  const { id, text, value, checked, onClick, hint, name } = props
+  const inputId = `${id}-${value}`
+
+  return (
+    <div className='govuk-radios__item'>
+      <input
+        id={inputId}
+        className='govuk-radios__input'
+        name={name}
+        type='radio'
+        value={value}
+        defaultChecked={checked}
+        onClick={onClick}
+        aria-describedby={`${inputId}-hint`}
+      />
+      <label className='govuk-label govuk-radios__label' htmlFor={inputId}>
+        {text}
+      </label>
+      {hint && (
+        <div id={`${inputId}-hint`} className="govuk-hint govuk-radios__hint">
+          {hint}
         </div>
       )}
     </div>
-  </fieldset>
-}
-
-export class RadioOption {
-  id: string;
-  text: string;
-  value: string;
-  checked: boolean;
-  onClick: function;
-  hint: ?string;
-
-  constructor (id: string, text: string, value: string, checked: boolean, onClick: function, hint: ?string) {
-    this.id = id
-    this.text = text
-    this.value = value
-    this.checked = checked
-    this.onClick = onClick
-    this.hint = hint
-  }
+  )
 }
