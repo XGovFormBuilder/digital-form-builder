74fd98c579db0cfdbd4c63befc6787896b9cac81
James Sheppard <57200592+james-sheppard-gov-uk@users.noreply.github.com>
2020-10-01 15:54:46 +0100

    config defined twice?!

--- designer/server/config.js
+++ designer/server/config.js
@@ -18,6 +18,7 @@ const config = {
   port: process.env.PORT,
   env: process.env.NODE_ENV,
   previewUrl: process.env.PREVIEW_URL || 'http://localhost:3009',
+  publishUrl: process.env.PUBLISH_URL || 'http://localhost:3009',  
   persistentBackend: process.env.PERSISTENT_BACKEND,
   persistentKeyId: process.env.PERSISTENT_KEY_ID,
   persistentAccessKey: process.env.PERSISTENT_ACCESS_KEY,
