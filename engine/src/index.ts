34380984c9fcc9bfefae6a3c557e28be5a002cce
bertrandmc <bemacode@gmail.com>
2020-10-21 14:01:47 +0100

    cleanup

--- engine/src/index.ts
+++ engine/src/index.ts
@@ -1,5 +1,4 @@
 export { RelativeUrl, FeedbackContextInfo, decode } from "./feedback";
-
 export { default as findByPostcode } from "./address-service";
 export { redirectTo, redirectUrl } from "./helpers";
 export { default as Model } from "./model";
