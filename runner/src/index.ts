b1210c5162bb4ce52a64fc2f53de5a96a81b2c7e
bertrandmc <bemacode@gmail.com>
2020-10-21 12:29:34 +0100

    Fix merge conflict

--- runner/src/index.ts
+++ runner/src/index.ts
@@ -4,10 +4,6 @@ createServer()
   .then((server) => server.start())
   .then(() => process.send && process.send("online"))
   .catch((err) => {
-<<<<<<< HEAD
     console.error(err);
-=======
-    console.log(err);
->>>>>>> 8a3127d1972ea8d17f598668dbc9b37d685c67e4
     process.exit(1);
   });
