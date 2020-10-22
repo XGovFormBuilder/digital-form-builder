6c2db198dd2b12012ee9df89ac5370ece0f1b9e4
bertrandmc <bemacode@gmail.com>
2020-10-22 14:04:58 +0100

    Adjust environment variable types to before Joi validation and refactor

--- runner/src/server/config.js
+++ runner/src/server/config.js
@@ -5,28 +5,33 @@ dotEnv.config({ path: ".env" });
 
 const minute = 60 * 1000;
 const DEFAULT_SESSION_TTL = 20 * minute;
+const DEFAULT_PORT = 3009;
+const DEFAULT_ENVIRONMENT = "development"
+const DEFAULT_LOG_LEVEL = "debug";
+const DEFAULT_SERVICE_URL = "http://localhost:3009"
+const DEFAULT_DOCUMENT_UPLOAD_API_URL = "http://localhost:9000"
 
 // Define config schema
 const schema = Joi.object({
-  port: Joi.number().default(3009),
+  port: Joi.number().default(DEFAULT_PORT),
   env: Joi.string()
     .valid("development", "test", "production")
-    .default("development"),
-  logLevel: Joi.string().optional(),
+    .default(DEFAULT_ENVIRONMENT),
+  logLevel: Joi.string().default(DEFAULT_LOG_LEVEL),
   ordnanceSurveyKey: Joi.string().optional(),
   browserRefreshUrl: Joi.string().optional(),
-  feedbackLink: Joi.string().optional(),
+  feedbackLink: Joi.string().default("#"),
   matomoId: Joi.string().optional(),
   matomoUrl: Joi.string().optional(),
   payApiUrl: Joi.string(),
   payReturnUrl: Joi.string(),
-  serviceUrl: Joi.string().optional(),
+  serviceUrl: Joi.string().default(DEFAULT_SERVICE_URL),
   redisHost: Joi.string().optional(),
   redisPort: Joi.number().optional(),
   redisPassword: Joi.string().optional(),
   redisTls: Joi.boolean().optional(),
   serviceName: Joi.string().optional(),
-  documentUploadApiUrl: Joi.string().optional(),
+  documentUploadApiUrl: Joi.string().default(DEFAULT_DOCUMENT_UPLOAD_API_URL),
   previewMode: Joi.boolean().optional(),
   sslKey: Joi.string().optional(),
   sslCert: Joi.string().optional(),
@@ -40,28 +45,27 @@ const schema = Joi.object({
 
 // Build config
 const config = {
-  port: process.env.PORT,
+  port: Number(process.env.PORT),
   env: process.env.NODE_ENV,
-  logLevel: process.env.LOG_LEVEL || "debug",
+  logLevel: process.env.LOG_LEVEL,
   ordnanceSurveyKey: process.env.ORDNANCE_SURVEY_KEY,
   browserRefreshUrl: process.env.BROWSER_REFRESH_URL,
-  feedbackLink: process.env.FEEDBACK_LINK || "#",
+  feedbackLink: process.env.FEEDBACK_LINK,
   matomoId: process.env.MATOMO_ID,
   matomoUrl: process.env.MATOMO_URL,
   payApiUrl: process.env.PAY_API_URL,
   payReturnUrl: process.env.PAY_RETURN_URL,
-  serviceUrl: process.env.SERVICE_URL || "http://localhost:3009",
+  serviceUrl: process.env.SERVICE_URL,
   redisHost: process.env.REDIS_HOST,
-  redisPort: process.env.REDIS_PORT,
+  redisPort: Number(process.env.REDIS_PORT),
   redisPassword: process.env.REDIS_PASSWORD,
   redisTls: process.env.REDIS_TLS === "true",
   serviceName: process.env.SERVICE_NAME,
-  documentUploadApiUrl:
-    process.env.DOCUMENT_UPLOAD_API_URL || "http://localhost:9000",
-  previewMode: process.env.PREVIEW_MODE || false,
+  documentUploadApiUrl: process.env.DOCUMENT_UPLOAD_API_URL,
+  previewMode: process.env.PREVIEW_MODE === "true",
   sslKey: process.env.SSL_KEY,
   sslCert: process.env.SSL_CERT,
-  sessionTimeout: process.env.SESSION_TIMEOUT,
+  sessionTimeout: Number(process.env.SESSION_TIMEOUT),
   sessionCookiePassword: process.env.SESSION_COOKIE_PASSWORD,
   rateLimit: process.env.RATE_LIMIT !== "false",
   fromEmailAddress: process.env.FROM_EMAIL_ADDRESS,
