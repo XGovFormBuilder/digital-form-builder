e3b0f320e8261e5385cf1aebe7192e2a83a27902
bertrandmc <bemacode@gmail.com>
2020-10-22 17:34:23 +0100

    More errors

--- engine/src/index.ts
+++ engine/src/index.ts
@@ -2,6 +2,6 @@ export { RelativeUrl, FeedbackContextInfo, decode } from "./feedback";
 
 export { default as findByPostcode } from "./address-service";
 export { redirectTo, redirectUrl } from "./helpers";
-export { default as Model } from "./model";
+export { default as Model } from "./model"
 export { default as Page } from "./page";
 export { plugin } from "./plugin";
