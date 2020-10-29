c4014b98535a4af8bc2b022a8bef957242b116d7
bertrandmc <bemacode@gmail.com>
2020-10-29 14:13:17 +0000

    Cleanup

--- model/src/values/index.ts
+++ model/src/values/index.ts
@@ -1,8 +1,8 @@
 import { ListRefValues } from "./list-ref-values";
 import { StaticValues } from "./static-values";
 
-export type ComponentValues = StaticValues | ListRefValues;
-
 export { yesNoValues } from "./yes-no-values";
 export { valuesFrom } from "./helpers";
 export { StaticValues } from "./static-values";
+
+export type ComponentValues = StaticValues | ListRefValues;
