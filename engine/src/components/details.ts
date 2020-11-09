a8c85951a68c95e4009aba055ce0eb13811f1809
bertrandmc <bemacode@gmail.com>
2020-11-09 13:50:59 +0000

    Use destruct

--- engine/src/components/details.ts
+++ engine/src/components/details.ts
@@ -2,10 +2,11 @@ import Component from "./component";
 
 export default class Details extends Component {
   getViewModel() {
-    const viewModel = super.getViewModel();
-
-    viewModel.summaryHtml = this.title;
-    viewModel.html = this.content;
+    const viewModel = {
+      ...super.getViewModel(),
+      summaryHtml: this.title,
+      html: this.content,
+    };
 
     if (this.options.condition) {
       viewModel.condition = this.options.condition;
