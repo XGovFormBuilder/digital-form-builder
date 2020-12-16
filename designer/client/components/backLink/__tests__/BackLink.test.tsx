1d0f092cc40726d0c44a13dfee7a2352352c05d6
bertrandmc <bemacode@gmail.com>
2020-12-16 16:10:55 +0000

    BackLink component

---
+++ designer/client/components/backLink/__tests__/BackLink.test.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { shallow } from "enzyme";
+import * as Code from "@hapi/code";
+import * as Lab from "@hapi/lab";
+import sinon from "sinon";
+
+const { expect } = Code;
+const lab = Lab.script();
+exports.lab = lab;
+const { test, describe, beforeEach } = lab;
+
+describe("BackLink Component", () => {
+  test("it renders correctly");
+
+  test("it passes href prop");
+
+  test("it passes onClick prop");
+});
