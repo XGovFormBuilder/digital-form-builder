484f19969835a8148274bf8f558e44db3eac26d6
Jonny Wyatt <jonnywyatt@yahoo.co.uk>
2020-12-17 22:48:26 +0000

    Use babel to compile server code & ts-node-dev to run; for client build, browserslist for targets and core-js for polyfills

--- designer/webpack.config.js
+++ designer/webpack.config.js
@@ -1,8 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const nodeExternals = require("webpack-node-externals");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const babelOptions = require("./.babelrc.js");
 const CopyPlugin = require("copy-webpack-plugin");
 
 const devMode = process.env.NODE_ENV !== "production";
@@ -25,16 +23,13 @@ const client = {
   node: {
     __dirname: false,
   },
-  devtool: "source-map",
+  devtool: "eval-cheap-module-source-map",
   module: {
     rules: [
       {
         test: /\.(js|jsx|tsx|ts)$/,
         exclude: /node_modules/,
         loader: "babel-loader",
-        options: {
-          ...babelOptions,
-        },
       },
       {
         test: /\.(sa|sc|c)ss$/,
@@ -94,39 +89,4 @@ const client = {
   },
 };
 
-const server = {
-  target: "node",
-  mode: environment,
-  watch: devMode,
-  entry: path.resolve(__dirname, "server", "index.ts"),
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "server.js",
-  },
-  resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
-    modules: [path.resolve(__dirname, "../node_modules")],
-  },
-  node: {
-    __dirname: false,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx|tsx|ts)$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-        options: {
-          ...babelOptions,
-        },
-      },
-    ],
-  },
-  externals: [
-    nodeExternals({
-      modulesDir: path.resolve(__dirname, "../node_modules"),
-    }),
-  ],
-};
-
-module.exports = [client, server];
+module.exports = client;
