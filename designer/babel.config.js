5ea6334ffd6de9cd0610cb102d24c91c9b96a682
jenbutongit <jen+git@cautionyourblast.com>
2020-12-18 16:36:17 +0000

    reverting changes

--- designer/babel.config.js
+++ designer/babel.config.js
@@ -6,16 +6,15 @@ const reactJsxPath = path.join(
 );
 
 module.exports = {
-  sourceType: "unambiguous", // https://github.com/webpack/webpack/issues/4039#issuecomment-564812879
   presets: [
     "@babel/typescript",
     "@babel/preset-react",
     [
       "@babel/preset-env",
       {
-        debug: false,
-        useBuiltIns: "usage",
-        corejs: 3,
+        targets: {
+          node: "12",
+        },
       },
     ],
   ],
