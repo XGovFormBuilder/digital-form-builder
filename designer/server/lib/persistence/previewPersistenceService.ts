f5188445fcae87d44ff9380da2b784feeafb0ab5
bertrandmc <bemacode@gmail.com>
2020-12-18 09:22:29 +0000

    Use publishURL config instead of previewUrl

--- designer/server/lib/persistence/previewPersistenceService.ts
+++ designer/server/lib/persistence/previewPersistenceService.ts
@@ -12,7 +12,7 @@ export class PreviewPersistenceService implements PersistenceService {
   logger: any;
 
   async uploadConfiguration(id: string, configuration: string) {
-    return Wreck.post(`${config.previewUrl}/publish`, {
+    return Wreck.post(`${config.publishUrl}/publish`, {
       payload: JSON.stringify({ id, configuration }),
     });
   }
@@ -23,12 +23,12 @@ export class PreviewPersistenceService implements PersistenceService {
   }
 
   async listAllConfigurations() {
-    const { payload } = await Wreck.get(`${config.previewUrl}/published`);
+    const { payload } = await Wreck.get(`${config.publishUrl}/published`);
     return JSON.parse(payload.toString());
   }
 
   async getConfiguration(id: string) {
-    const { payload } = await Wreck.get(`${config.previewUrl}/published/${id}`);
+    const { payload } = await Wreck.get(`${config.publishUrl}/published/${id}`);
     return payload.toString();
   }
 }
