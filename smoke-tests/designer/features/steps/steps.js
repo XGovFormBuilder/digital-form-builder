41c19d07385fd113070c50ba5628678ca81b0719
Carlos Gutierrez <carlos@spartan-testsolutions.co.uk>
2021-01-08 13:34:26 +0000

    Assert value is clear before setting new value

--- smoke-tests/designer/features/steps/steps.js
+++ smoke-tests/designer/features/steps/steps.js
@@ -123,6 +123,7 @@ When("I edit the page title on the {string}", (pageName) => {
   this.newPageName = "testing";
   FormDesignerPage.editPageForPageName(pageName).click();
   EditPageSection.pageTitle.clearValue();
+  expect(EditPageSection.pageTitle).toHaveValue("");
   EditPageSection.pageTitle.setValue(this.newPageName);
   EditPageSection.saveBtn.click();
 });
