3c222759e2e940ac46cc1178ccb10229ac0d25d5
bertrandmc <bemacode@gmail.com>
2021-01-11 10:25:56 +0000

    Focus trap navigation in flyout

--- designer/client/flyout.js
+++ designer/client/flyout.js
@@ -1,4 +1,5 @@
 import React, { useContext, useLayoutEffect, useState } from "react";
+import FocusTrap from "focus-trap-react";
 import { FlyoutContext } from "./context";
 import { i18n } from "./i18n";
 
@@ -54,27 +55,32 @@ function Flyout(props) {
   const { style, width = "", onHide } = useFlyoutEffect(props);
 
   return (
-    <div className="flyout-menu show">
-      <div className={`flyout-menu-container ${width}`} style={style}>
-        <a
-          title="Close"
-          className="close govuk-body govuk-!-font-size-16"
-          onClick={onHide}
-        >
-          {i18n("close")}
-        </a>
-        <div className="panel panel--flyout">
-          <div className="panel-header govuk-!-padding-top-4 govuk-!-padding-left-4">
-            {props.title && <h4 className="govuk-heading-m">{props.title}</h4>}
-          </div>
-          <div className="panel-body">
-            <div className="govuk-!-padding-left-4 govuk-!-padding-right-4 govuk-!-padding-bottom-4">
-              {props.children}
+    <FocusTrap>
+      <div className="flyout-menu show">
+        <div className={`flyout-menu-container ${width}`} style={style}>
+          <a
+            tabIndex="0"
+            title="Close"
+            className="close govuk-body govuk-!-font-size-16 govuk-link"
+            onClick={onHide}
+          >
+            {i18n("close")}
+          </a>
+          <div className="panel panel--flyout">
+            <div className="panel-header govuk-!-padding-top-4 govuk-!-padding-left-4">
+              {props.title && (
+                <h4 className="govuk-heading-m">{props.title}</h4>
+              )}
+            </div>
+            <div className="panel-body">
+              <div className="govuk-!-padding-left-4 govuk-!-padding-right-4 govuk-!-padding-bottom-4">
+                {props.children}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </FocusTrap>
   );
 }
 
