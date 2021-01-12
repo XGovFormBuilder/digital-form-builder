76a8519abb399cfbab65a10164357e6c81ee20b0
bertrandmc <bemacode@gmail.com>
2021-01-12 14:16:48 +0000

    adjust import

--- designer/client/index.tsx
+++ designer/client/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Menu from "./menu";
-import { Visualisation } from "./components/visualisation";
+import { Visualisation } from "./components/Visualisation";
 import { NewConfig } from "./components/newConfig";
 import { Data } from "@xgovformbuilder/model";
 import { customAlphabet } from "nanoid";
