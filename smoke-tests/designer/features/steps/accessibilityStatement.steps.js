6bedbe429b6a5826fd1b33823c5bfa41e6a94d8b
Carlos Gutierrez <carlos@spartan-testsolutions.co.uk>
2021-01-14 15:03:39 +0000

    Updated two steps to check for presence of the phase banner

--- smoke-tests/designer/features/steps/accessibilityStatement.steps.js
+++ smoke-tests/designer/features/steps/accessibilityStatement.steps.js
@@ -1,10 +1,12 @@
 const { Given, When, Then } = require("cucumber");
 const Config = require("../pageobjects/pages/config.page");
 const actions = require("../actions/actions");
-const formDesigner = require("../pageobjects/pages/formDesigner.page");
 
 Given("I am on the new configuration page", function () {
   Config.open();
+  expect(Config.govPhaseBanner).toHaveText(
+    "ALPHA This service is currently in development"
+  );
 });
 
 Then("I can see the footer element at the bottom of the page", function () {
@@ -13,6 +15,9 @@ Then("I can see the footer element at the bottom of the page", function () {
 
 Given("I am on the form designer page", function () {
   actions.createNewConfig();
+  expect(Config.govPhaseBanner).toHaveText(
+    "ALPHA This service is currently in development"
+  );
 });
 
 When("I select the {string} in the footer", function (footerLink) {
