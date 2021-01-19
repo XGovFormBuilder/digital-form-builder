a5b6ffc5753fe368c133fa596e92dd2868e1fce7
Richard Oliver Bray <richardoliverbray@Richards-MBP.lan>
2021-01-19 17:27:19 +0000

    config(designer): update test config files to support styles
    This also changes the testMach location to run tests in the whole
    application and not just the conditions file.

--- designer/jest.config.js
+++ designer/jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   roots: ["<rootDir>/client"],
-  testMatch: ["<rootDir>/client/conditions/__tests__/*.jest.(ts|tsx)"],
+  testMatch: ["<rootDir>/client/**/__tests__/*.(jest|test).(ts|tsx)"],
   testPathIgnorePatterns: ["<rootDir>/test/"],
   setupFilesAfterEnv: ["<rootDir>/jest-setup.js"],
+  moduleNameMapper: {
+    "\\.(css|scss)$": "<rootDir>/__mocks__/styleMock.js",
+  },
 };
