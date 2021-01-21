01436b9e77887cd7bd6c12949f5877bcf17336fa
jenbutongit <jen+git@cautionyourblast.com>
2021-01-21 13:01:20 +0000

    use nanoid

--- smoke-tests/designer/features/actions/actions.js
+++ smoke-tests/designer/features/actions/actions.js
@@ -3,11 +3,12 @@ const ConfigPage = require("../pageobjects/pages/config.page");
 const FormDesigner = require("../pageobjects/pages/formDesigner.page");
 const FieldData = require("../../data/componentFieldData");
 const { toCamelCase } = require("../../support/testHelpers");
+const { nanoid } = require("nanoid");
 
 class Actions {
   createNewConfig() {
     ConfigPage.open();
-    this.configRef = `smoke-testing ${Date.parse(Date())}`;
+    this.configRef = `smoke-testing ${nanoid(10)}`;
     ConfigPage.newConfig(this.configRef);
     expect(browser).toHaveUrlContaining(this.configRef.replace(" ", "-"));
   }
