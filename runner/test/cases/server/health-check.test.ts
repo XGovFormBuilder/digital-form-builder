67b65e58f5dfbd016a3a31af35e87d913c1456c3
bertrandmc <bemacode@gmail.com>
2021-01-25 17:05:13 +0000

    Tests

---
+++ runner/test/cases/server/health-check.test.ts
@@ -0,0 +1,32 @@
+import Lab from "@hapi/lab";
+import { execSync } from "child_process";
+import { expect } from "@hapi/code";
+import createServer from "src/server";
+
+const LAST_COMMIT = execSync("git rev-parse HEAD").toString().trim();
+
+const { before, test, suite, after } = (exports.lab = Lab.script());
+
+suite(`/health-check Route`, () => {
+  let server;
+
+  before(async () => {
+    server = await createServer({});
+    await server.start();
+  });
+
+  after(async () => {
+    await server.stop();
+  });
+
+  test("/health-check route response is correct", async () => {
+    const options = {
+      method: "GET",
+      url: "/heath-check",
+    };
+
+    const { result } = await server.inject(options);
+    expect(result.lastCommit).to.equal(LAST_COMMIT);
+    expect(result.time).to.be.a.string();
+  });
+});
