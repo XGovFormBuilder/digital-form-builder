aff5ca4a11d9cc4071f5a8c0d80c134b78ac29a7
Jen Duong <jen+git@cautionyourblast.com>
2021-01-25 16:14:23 +0000

    Bug/escape urls STP-761  (#280)
    * refactor routes
    
    * types, escape test (wip)
    
    * test encoding
    
    * rename route
    
    * arkit
    
    * refactor to jest
    
    * better matcher

---
+++ designer/server/plugins/routes/index.ts
@@ -0,0 +1,5 @@
+import * as newConfig from "./newConfig";
+import * as api from "./api";
+import * as configurations from "./configurations";
+
+export { newConfig, api, configurations };
---
+++ designer/server/plugins/routes/newConfig.ts
@@ -0,0 +1,67 @@
+import config from "../../config";
+import newFormJson from "../../../new-form.json";
+import { publish } from "../../lib/publish";
+import { ServerRoute } from "@hapi/hapi";
+import { FormConfiguration } from "@xgovformbuilder/model";
+
+export const get: ServerRoute = {
+  method: "get",
+  path: "/new",
+  options: {
+    handler: async (request, h) => {
+      const { persistenceService } = request.services([]);
+      let configurations: FormConfiguration[] = [];
+      try {
+        configurations = await persistenceService.listAllConfigurations();
+        return h.view("designer", {
+          newConfig: true,
+          configurations,
+          phase: config.phase,
+        });
+      } catch (e) {
+        configurations = [];
+        return h.view("designer", {
+          newConfig: true,
+          configurations,
+          error: e,
+          phase: config.phase,
+        });
+      }
+    },
+  },
+};
+
+export const registerNewFormWithRunner: ServerRoute = {
+  method: "post",
+  path: "/new",
+  options: {
+    handler: async (request, h) => {
+      const { persistenceService } = request.services([]);
+      const { selected, name } = request.payload;
+      const newName = encodeURIComponent(name);
+
+      try {
+        if (selected.Key === "New") {
+          if (config.persistentBackend !== "preview") {
+            await persistenceService.uploadConfiguration(
+              `${newName}.json`,
+              JSON.stringify(newFormJson)
+            );
+          }
+          await publish(newName, newFormJson);
+        } else {
+          await persistenceService.copyConfiguration(
+            `${selected.Key}`,
+            newName
+          );
+          const copied = await persistenceService.getConfiguration(newName);
+          await publish(newName, copied);
+        }
+      } catch (e) {
+        console.error(e);
+      }
+
+      return h.redirect(`/${newName}`);
+    },
+  },
+};
