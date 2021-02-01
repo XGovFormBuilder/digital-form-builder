41a73517941b433f785aa9b3f74cfe3b89a93114
Bertrandmc <bemacode@gmail.com>
2021-02-01 11:10:56 +0000

    Stp 1141 date field in edit conditions (#300)
    * cleanup
    
    * update yarn lock
    
    * adjust type
    
    * validate condition name
    
    * Adjust date format and improe usability
    
    * translation
    
    * adjust errors
    
    * Implement ErrorMessage component and refactor to remove duplication
    
    * Fix tests
    
    * cleanup
    
    * Adjust test labels
    
    * Adjust test labels
    
    * Test ErrorMessage component
    
    * Cleanup
    
    * Deprecate snapshot and fix type

--- designer/client/conditions/__tests__/AbsoluteDateValues.jest.tsx
+++ designer/client/conditions/__tests__/AbsoluteDateValues.jest.tsx
@@ -16,17 +16,17 @@ describe("AbsoluteDateValues", () => {
         value={{ year: 1999, month: 12, day: 31 }}
       />
     );
-    expect(await findInputValue("yyyy")).toEqual("1999");
-    expect(await findInputValue("MM")).toEqual("12");
-    expect(await findInputValue("dd")).toEqual("31");
+    expect(await findInputValue("Year")).toEqual("1999");
+    expect(await findInputValue("Month")).toEqual("12");
+    expect(await findInputValue("Day")).toEqual("31");
   });
 
   it("calls the updateValue prop if a valid date is entered", async () => {
     const updateValue = jest.fn();
     render(<AbsoluteDateValues updateValue={updateValue} value={{}} />);
-    await typeIntoInput("yyyy", "2020");
-    await typeIntoInput("MM", "4");
-    await typeIntoInput("dd", "26");
+    await typeIntoInput("Year", "2020");
+    await typeIntoInput("Month", "4");
+    await typeIntoInput("Day", "26");
     expect(updateValue).toHaveBeenCalledWith({ year: 2020, month: 4, day: 26 });
   });
 
@@ -38,45 +38,45 @@ describe("AbsoluteDateValues", () => {
         value={{ year: 1999, month: 12, day: 31 }}
       />
     );
-    await typeIntoInput("yyyy", "2020");
-    await typeIntoInput("MM", "4");
-    await typeIntoInput("dd", "26");
+    await typeIntoInput("Year", "2020");
+    await typeIntoInput("Month", "4");
+    await typeIntoInput("Day", "26");
     expect(updateValue).toHaveBeenCalledWith({ year: 2020, month: 4, day: 26 });
   });
 
   it("doesn't call the updateValue prop if an valid day is not entered", async () => {
     const updateValue = jest.fn();
     render(<AbsoluteDateValues updateValue={updateValue} value={{}} />);
-    await typeIntoInput("yyyy", "2020");
-    await typeIntoInput("MM", "4");
-    await typeIntoInput("dd", "0");
+    await typeIntoInput("Year", "2020");
+    await typeIntoInput("Month", "4");
+    await typeIntoInput("Day", "0");
     expect(updateValue).not.toHaveBeenCalled();
   });
 
   it("doesn't call the updateValue prop if no day is entered", async () => {
     const updateValue = jest.fn();
     render(<AbsoluteDateValues updateValue={updateValue} value={{}} />);
-    await typeIntoInput("yyyy", "2020");
-    await typeIntoInput("MM", "4");
-    await typeIntoInput("dd", "");
+    await typeIntoInput("Year", "2020");
+    await typeIntoInput("Month", "4");
+    await typeIntoInput("Day", "");
     expect(updateValue).not.toHaveBeenCalled();
   });
 
   it("doesn't call the updateValue prop if no month is entered", async () => {
     const updateValue = jest.fn();
     render(<AbsoluteDateValues updateValue={updateValue} value={{}} />);
-    await typeIntoInput("yyyy", "2020");
-    await typeIntoInput("MM", "");
-    await typeIntoInput("dd", "7");
+    await typeIntoInput("Year", "2020");
+    await typeIntoInput("Month", "");
+    await typeIntoInput("Day", "7");
     expect(updateValue).not.toHaveBeenCalled();
   });
 
   it("doesn't call the updateValue prop if no year is entered", async () => {
     const updateValue = jest.fn();
     render(<AbsoluteDateValues updateValue={updateValue} value={{}} />);
-    await typeIntoInput("yyyy", "");
-    await typeIntoInput("MM", "4");
-    await typeIntoInput("dd", "23");
+    await typeIntoInput("Year", "");
+    await typeIntoInput("Month", "4");
+    await typeIntoInput("Day", "23");
     expect(updateValue).not.toHaveBeenCalled();
   });
 });
