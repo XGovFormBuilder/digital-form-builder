1bae38345244121e632f392dd777c39b98a4dcef
Bertrandmc <bemacode@gmail.com>
2021-02-04 12:48:56 +0000

    Add Link menu with i18n

--- designer/client/menu.js
+++ designer/client/menu.js
@@ -14,6 +14,7 @@ import { FormDetails } from "./components/FormDetails";
 import { ListContextProvider } from "./reducers/listReducer";
 import { ListsEditorContextProvider } from "./reducers/list/listsEditorReducer";
 import { DataContext } from "./context";
+import { i18n } from "./i18n";
 
 export default class Menu extends React.Component {
   static contextType = DataContext;
@@ -163,7 +164,7 @@ export default class Menu extends React.Component {
         )}
         {this.state.showAddLink && (
           <Flyout
-            title="Add Link"
+            title={i18n("menu.links")}
             onHide={() => this.setState({ showAddLink: false })}
           >
             <LinkCreate
