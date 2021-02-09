3a32e57241608a93cec6b659ccc5d794d596896e
Bertrandmc <bemacode@gmail.com>
2021-02-09 15:27:20 +0000

    cleanup

--- designer/client/components/ComponentCreate/ComponentCreate.tsx
+++ designer/client/components/ComponentCreate/ComponentCreate.tsx
@@ -31,12 +31,13 @@ function useComponentCreate(props) {
   const hasErrors = hasValidationErrors(errors);
 
   useEffect(() => {
-    let isMounted = true;
     // render in the next re-paint to allow the DOM to reflow without the list
     // thus resetting the Flyout wrapper scrolling position
     // This is a quick work around the bug in small screens
     // where once user scrolls down the components list and selects one of the bottom components
     // then the component edit screen renders already scrolled to the bottom
+    let isMounted = true;
+
     if (selectedComponent?.type) {
       window.requestAnimationFrame(() => {
         if (isMounted) setRenderTypeEdit(true);
