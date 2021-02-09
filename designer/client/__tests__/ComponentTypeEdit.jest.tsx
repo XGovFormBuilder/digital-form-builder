b2288811ab5f4729ffc1c9665d374bd841800c76
Bertrandmc <bemacode@gmail.com>
2021-02-09 15:26:06 +0000

    Test YesNo field text changes

--- designer/client/__tests__/ComponentTypeEdit.jest.tsx
+++ designer/client/__tests__/ComponentTypeEdit.jest.tsx
@@ -307,4 +307,64 @@ describe("ComponentTypeEdit", () => {
       expect(getByText(text)).toBeInTheDocument();
     });
   });
+
+  describe("YesNo", () => {
+    let stateProps;
+
+    beforeEach(() => {
+      stateProps = {
+        component: {
+          type: "YesNoField",
+          name: "TestYesNo",
+          options: {},
+        },
+      };
+    });
+
+    test("title input hint text is rendered correctly", () => {
+      const { getByText } = render(
+        <RenderWithContext stateProps={stateProps}>
+          <ComponentTypeEdit page={mockData.pages[0]} />
+        </RenderWithContext>
+      );
+
+      const text = "Enter the name to show for this field";
+      expect(getByText(text)).toBeInTheDocument();
+    });
+
+    test("help text input hint text is rendered correctly", () => {
+      const { getByText } = render(
+        <RenderWithContext stateProps={stateProps}>
+          <ComponentTypeEdit page={mockData.pages[0]} />
+        </RenderWithContext>
+      );
+
+      const text = "Enter the description to show for this field";
+      expect(getByText(text)).toBeInTheDocument();
+    });
+
+    test("hide title checkbox hint text is rendered correctly", () => {
+      const { getByText } = render(
+        <RenderWithContext stateProps={stateProps}>
+          <ComponentTypeEdit page={mockData.pages[0]} />
+        </RenderWithContext>
+      );
+
+      const text =
+        "Tick this box if you do not want the title to show on the page";
+      expect(getByText(text)).toBeInTheDocument();
+    });
+
+    test("component name input hint text is rendered correctly", () => {
+      const { getByText } = render(
+        <RenderWithContext stateProps={stateProps}>
+          <ComponentTypeEdit page={mockData.pages[0]} />
+        </RenderWithContext>
+      );
+
+      const text =
+        "This is generated automatically and does not show on the page. Only change it if you are using an integration that requires you to, for example GOV.UK Notify. It must not contain spaces.";
+      expect(getByText(text)).toBeInTheDocument();
+    });
+  });
 });
