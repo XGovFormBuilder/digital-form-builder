e5a50b5dd3312ea1575df2ab7e5899df91c694cc
Bertrandmc <bemacode@gmail.com>
2021-02-10 13:27:44 +0000

    Adjust test page length

--- smoke-tests/designer/features/steps/steps.js
+++ smoke-tests/designer/features/steps/steps.js
@@ -209,7 +209,7 @@ Then("the {string} is no longer visible in the designer", (pageName) => {
   FormDesignerPage.formPageTitles.forEach((elem) => {
     pageNames.push(elem.getText());
   });
-  expect(FormDesignerPage.formPages.length).toEqual(1);
+  expect(FormDesignerPage.formPages.length).toEqual(3);
   chai.expect(pageNames).not.include(pageName);
 });
 
