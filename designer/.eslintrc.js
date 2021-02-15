652364cb0bdf61f0ead9646e746fbd88d1d6e6fb
jenbutongit <jen+git@cautionyourblast.com>
2021-02-15 17:25:30 +0000

    sorted keys

--- designer/.eslintrc.js
+++ designer/.eslintrc.js
@@ -16,10 +16,11 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: "module",
   },
-  plugins: ["react", "react-hooks"],
+  plugins: ["react", "react-hooks", "i18n-json"],
   rules: {
     "react/prop-types": 0,
     "react-hooks/rules-of-hooks": "warn",
+    "i18n-json/sorted-keys": 2,
   },
   settings: {
     react: {
