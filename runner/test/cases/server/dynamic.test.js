b1c6e8982c400930d70e32db763f4513d9ebd13b
Bertrandmc <bemacode@gmail.com>
2021-02-17 15:53:47 +0000

    Remove error

--- runner/test/cases/server/dynamic.test.js
+++ runner/test/cases/server/dynamic.test.js
@@ -119,7 +119,7 @@ suite("Dynamic pages", { skip: true }, () => {
       })
     );
     expect(response.headers.location).to.equal(
-      `XXX/dynamic/applicant-repeatable?num=1&visit=${VISIT_ID}`
+      `/dynamic/applicant-repeatable?num=1&visit=${VISIT_ID}`
     );
   });
 
