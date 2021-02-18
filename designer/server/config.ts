42f2b0d586b2f7c9d04ba8004d33396869a7224b
Bertrandmc <bemacode@gmail.com>
2021-02-18 16:47:29 +0000

    Create .env for runner and designer with LAST_COMMIT and LAST_TAG variables (#404)
    * Create .env in branch
    
    * Try only with .env
    
    * Adjust designer .env path
    
    * Create .env in mater workflow

--- designer/server/config.ts
+++ designer/server/config.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import joi from "joi";
 
-dotenv.config({ path: "../.env" });
+dotenv.config({ path: ".env" });
 
 export interface Config {
   env: "development" | "test" | "production";
