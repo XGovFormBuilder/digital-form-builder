0b99ff3c4e95c7509214565ab4762898c1372a32
Enda McCormack <endamccormack0@gmail.com>
2021-02-19 13:44:52 +0000

    refactored tests to match updated pattern

--- designer/client/__tests__/page-create.jest.tsx
+++ designer/client/__tests__/page-create.jest.tsx
@@ -2,69 +2,39 @@ import React from "react";
 import { render } from "@testing-library/react";
 import PageCreate from "../page-create";
 
-describe("page create", () => {
-  describe("Page create fields", () => {
-    let props;
-    let getByText;
+describe("page create fields text", () => {
+  test("displays field titles and help texts", () => {
+    const props = {
+      path: "/some-path",
+      data: {
+        sections: [],
+        pages: [],
+      },
+    };
 
-    beforeEach(() => {
-      props = {
-        path: "/some-path",
-        data: {
-          sections: [],
-          pages: [],
-        },
-      };
-
-      getByText = render(<PageCreate {...props} />).getByText;
-    });
-
-    test("display page type", () => {
-      const text = "Page type";
-      expect(getByText(text)).toBeInTheDocument();
-    });
-
-    test("display page type help text", () => {
-      const text =
-        "Select a Start Page to start a form, a Question Page to request information, and a Summary Page at the end of a section or form. For example, use a Summary to let users check their answers to questions before they submit the form.";
-      expect(getByText(text)).toBeInTheDocument();
-    });
-
-    test("display link from title", () => {
-      const text = "Page type";
-      expect(getByText(text)).toBeInTheDocument();
-    });
-
-    test("display link from help text", () => {
-      const text = "Add a link to this page from a different page in the form.";
-      expect(getByText(text)).toBeInTheDocument();
-    });
-
-    test("display page title's title", () => {
-      const text = "Page title";
-      expect(getByText(text)).toBeInTheDocument();
-    });
-
-    test("display path title", () => {
-      const text = "Path";
-      expect(getByText(text)).toBeInTheDocument();
-    });
-
-    test("display path help text", () => {
-      const text =
-        "Appears in the browser path. The value you enter in the page title field automatically populates the path name. To override it, enter your own path name, relevant to the page, and use lowercase text and hyphens between words. For example, '/personal-details'.";
-      expect(getByText(text)).toBeInTheDocument();
-    });
-
-    test("display section option title", () => {
-      const text = "Section (optional)";
-      expect(getByText(text)).toBeInTheDocument();
-    });
-
-    test("display section option help text", () => {
-      const text =
-        "Use sections to split a form. For example, to add a section per applicant. The section title appears above the page title. However, if these titles are the same, the form will only show the page title.";
-      expect(getByText(text)).toBeInTheDocument();
-    });
+    const { getByText } = render(<PageCreate {...props} />);
+    expect(getByText("Page type")).toBeInTheDocument();
+    expect(
+      getByText(
+        "Select a Start Page to start a form, a Question Page to request information, and a Summary Page at the end of a section or form. For example, use a Summary to let users check their answers to questions before they submit the form."
+      )
+    ).toBeInTheDocument();
+    expect(getByText("Link from (optional)")).toBeInTheDocument();
+    expect(
+      getByText("Add a link to this page from a different page in the form.")
+    ).toBeInTheDocument();
+    expect(getByText("Page title")).toBeInTheDocument();
+    expect(getByText("Path")).toBeInTheDocument();
+    expect(
+      getByText(
+        "Appears in the browser path. The value you enter in the page title field automatically populates the path name. To override it, enter your own path name, relevant to the page, and use lowercase text and hyphens between words. For example, '/personal-details'."
+      )
+    ).toBeInTheDocument();
+    expect(getByText("Section (optional)")).toBeInTheDocument();
+    expect(
+      getByText(
+        "Use sections to split a form. For example, to add a section per applicant. The section title appears above the page title. However, if these titles are the same, the form will only show the page title."
+      )
+    ).toBeInTheDocument();
   });
 });
