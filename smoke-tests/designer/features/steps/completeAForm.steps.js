16f4f3a02752f98b89a01272ba4d729cc27821bb
Carlos Gutierrez <carlos@spartan-testsolutions.co.uk>
2021-03-11 12:59:28 +0000

    Changed steps to latest syntax

--- smoke-tests/designer/features/steps/completeAForm.steps.js
+++ smoke-tests/designer/features/steps/completeAForm.steps.js
@@ -3,41 +3,37 @@ const { formRunner } = require("../pageobjects/pages");
 
 Given("I am at the beginning of the {string} form", function (formName) {
   let formPath = formName.replace(/ /g, "-").toLowerCase();
-  console.log(formPath);
   formRunner.open(formPath);
 });
 
-Given(/^I complete the form$/, function () {
+Given("I complete the form", function () {
   formRunner.selectRadio("Yes, I do have a link");
   formRunner.multilineText("https://nodejs.org/en/");
   formRunner.selectRadio("No, I don't have evidence");
   formRunner.multilineText("File upload is not yet finished!!");
 });
 
-When(/^I view the Summary page$/, function () {
+When("I view the Summary page", function () {
   expect(formRunner.pageTitle).toHaveText("summary");
 });
 
-When(/^I submit the completed form$/, function () {
+When("I submit the completed form", function () {
   formRunner.continueButton.click();
 });
 
-Then(/^the "([^"]*)" page is displayed$/, function (message) {
-  expect(formRunner.submissionConfirmation).toHaveText(message);
+Then("the {string} page is displayed", function (successMessage) {
+  expect(formRunner.submissionConfirmation).toHaveText(successMessage);
 });
 
-When(/^I choose "([^"]*)"$/, function (usersChoice) {
+When("I choose {string}", function (usersChoice) {
   formRunner.selectRadio(usersChoice);
 });
 
-Then(/^I taken directly to the page titled "([^"]*)"$/, function (pageTitle) {
+Then("I taken directly to the page titled {string}", function (pageTitle) {
   expect(formRunner.pageTitle).toHaveTextContaining(pageTitle);
 });
 
-Given(
-  /^I have progressed to the Do you have any evidence\? page$/,
-  function () {
-    formRunner.selectRadio("Yes, I do have a link");
-    formRunner.multilineText("https://nodejs.org/en/");
-  }
-);
+Given("I have progressed to the Do you have any evidence? page", function () {
+  formRunner.selectRadio("Yes, I do have a link");
+  formRunner.multilineText("https://nodejs.org/en/");
+});
