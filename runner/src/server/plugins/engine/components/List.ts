fd28d8fee90bfd51ae76ecfc68b12a22f671d313
jenbutongit <jen+git@cautionyourblast.com>
2021-03-16 17:27:52 +0000

    revert to select field, fix list component

--- runner/src/server/plugins/engine/components/List.ts
+++ runner/src/server/plugins/engine/components/List.ts
@@ -1,19 +1,29 @@
 import { ComponentBase } from "./ComponentBase";
 import { FormData, FormSubmissionErrors } from "../types";
+import { Item } from "@xgovformbuilder/model";
 
 export class List extends ComponentBase {
+  list: List;
+  get items(): Item[] {
+    return this.list?.items ?? [];
+  }
+
+  constructor(def, model) {
+    super(def, model);
+    this.list = model.getList(def.list);
+  }
+
   getViewModel(formData: FormData, errors: FormSubmissionErrors) {
-    // @ts-ignore
-    const { values, options } = this;
+    const { items, options } = this;
     const viewModel = super.getViewModel(formData, errors);
 
     if ("type" in options && options.type) {
       viewModel.type = options.type;
     }
 
-    viewModel.content = values?.items.map((item) => {
+    viewModel.content = items.map((item) => {
       const contentItem: { text: string; condition?: any } = {
-        text: item.hint || item.label,
+        text: item.text,
       };
       if (item.condition) {
         contentItem.condition = item.condition;
