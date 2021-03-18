b3724178ceb1554cd927ddc55bd293e7136ba6d9
jenbutongit <jen+git@cautionyourblast.com>
2021-03-18 15:14:59 +0000

    not sure what I did

--- runner/src/server/plugins/engine/pageControllers/PageControllerBase.ts
+++ runner/src/server/plugins/engine/pageControllers/PageControllerBase.ts
@@ -363,6 +363,8 @@ export class PageControllerBase {
         }
         // apply condition to items for radios, checkboxes etc
         const items = evaluatedComponent.model.items;
+        console.log(items);
+
         if (items instanceof Array) {
           evaluatedComponent.model.items = items.filter((item) =>
             item.condition
@@ -370,8 +372,11 @@ export class PageControllerBase {
               : true
           );
         }
+
         return evaluatedComponent;
       });
+
+      console.log(viewModel);
 
       const lastVisited = progress[progress.length - 1];
       if (!lastVisited || !lastVisited.startsWith(currentPath)) {
