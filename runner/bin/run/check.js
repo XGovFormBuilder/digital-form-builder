40a9cf0525e898142234dd98ffc82cb854aaaa0a
jenbutongit <jen+git@cautionyourblast.com>
2021-03-18 11:51:55 +0000

    add process.exit

--- runner/bin/run/check.js
+++ runner/bin/run/check.js
@@ -25,6 +25,7 @@ async function check() {
         ", "
       )} is/are out of date. Use the designer to upload your files, which runs the migration scripts. Download those JSONs to replace the outdated forms. Migration scripts will not cover conditional reveal fields. You will need to fix those manually.`
     );
+    process.exit(1);
   } else {
     cli.info("Your forms are up to date");
   }
