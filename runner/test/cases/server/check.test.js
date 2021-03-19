9a89d22d700ee7568ab4c220d770301538a231f2
jenbutongit <jen+git@cautionyourblast.com>
2021-03-19 11:55:30 +0000

    remove unused plugin, check tests wip

--- runner/test/cases/server/check.test.js
+++ runner/test/cases/server/check.test.js
@@ -4,11 +4,13 @@ import sinon from "sinon";
 const lab = Lab.script();
 exports.lab = lab;
 const { expect } = Code;
-const { describe, test, after } = lab;
+const { describe, test, afterEach } = lab;
 import fs from "fs";
 import * as jsonHelper from "../../../bin/run/check/getJsonFiles";
 import * as outOfDateHelper from "../../../bin/run/check/getOutOfDateForms";
-describe("check out of date forms", () => {
+import * as cliux from "cli-ux";
+
+describe.only("check out of date forms", () => {
   test("getJsonFiles returns files with .json extension only", async () => {
     const files = await jsonHelper.getJsonFiles();
     expect(files).to.contain(["report-a-terrorist.json", "test.json"]);
@@ -37,7 +39,27 @@ describe("check out of date forms", () => {
     ]);
   });
 
-  after(() => {
+  test("process exits with code 1 when there is an out of date form", async () => {
+    const cliSpy = sinon.spy(cliux.cli, "warn");
+    const processSpy = sinon.spy(process, "exit");
+    const fake = sinon.fake.returns([
+      "no-version.json",
+      "v0.json",
+      "v1.json",
+      "v2.json",
+    ]);
+
+    sinon.replace(jsonHelper, "getJsonFiles", fake);
+    const fsStub = sinon.stub(fs.promises, "readFile");
+    fsStub.onCall(0).returns("{}");
+    fsStub.onCall(1).returns(`{ "version": 0 }`);
+
+    await check();
+
+    expect(cliSpy).to.be.called(1);
+  });
+
+  afterEach(() => {
     sinon.restore();
   });
 });
