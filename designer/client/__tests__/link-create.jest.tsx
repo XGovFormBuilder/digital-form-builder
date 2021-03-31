decef7879362c3127667c7b15e9ff4a066a1fa8e
jenbutongit <jen+git@cautionyourblast.com>
2021-03-31 11:17:31 +0100

    fix tests

--- designer/client/__tests__/link-create.jest.tsx
+++ designer/client/__tests__/link-create.jest.tsx
@@ -1,40 +1,36 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import LinkCreate from "../link-create";
+import { Data } from "@xgovformbuilder/model";
+import { DataContext } from "../context";
+
+const dataValue = {
+  data: new Data({
+    lists: [],
+    pages: [],
+    sections: [],
+    startPage: "",
+    conditions: [],
+  }),
+  save: jest.fn(),
+};
+export const customRender = (children, providerProps = dataValue) => {
+  return render(
+    <DataContext.Provider value={providerProps}>
+      {children}
+      <div id="portal-root" />
+    </DataContext.Provider>
+  );
+};
 
 describe("LinkCreate", () => {
-  let data: any;
-
-  beforeEach(() => {
-    data = {
-      inputsAccessibleAt: jest.fn().mockReturnValue([]),
-      allInputs: jest.fn().mockReturnValue([]),
-      valuesFor: jest.fn(),
-      hasConditions: false,
-      conditions: [],
-      pages: [
-        {
-          title: "First page",
-          path: "/first-page",
-          components: [],
-        },
-        {
-          title: "Summary",
-          path: "/summary",
-          controller: "./pages/summary.js",
-          components: [],
-        },
-      ],
-    };
-  });
-
   test("hint texts are rendered correctly", () => {
     const hint1 =
       "You can add links between different pages and set conditions for links to control the page that loads next. For example, a question page with a component for yes and no options could have link conditions based on which option a user selects.";
     const hint2 =
       "To add a link in the main screen, click and hold the title of a page and drag a line to the title of the page you want to link it to. To edit a link, select its line.";
 
-    const { getByText } = render(<LinkCreate data={data} />);
+    const { getByText } = customRender(<LinkCreate />);
     expect(getByText(hint1)).toBeInTheDocument();
     expect(getByText(hint2)).toBeInTheDocument();
   });
@@ -43,7 +39,7 @@ describe("LinkCreate", () => {
     const hint =
       "You cannot add any conditions as there are no components on the page you wish to link from. Add a component, such as an Input or a Selection field, and then add a condition.";
 
-    const { getByLabelText, getByText } = render(<LinkCreate data={data} />);
+    const { getByLabelText, getByText } = customRender(<LinkCreate />);
 
     fireEvent.change(getByLabelText("From"), {
       target: { value: data.pages[0].path },
