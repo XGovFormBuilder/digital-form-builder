169ae1f07a1af6a417b79e27a7e4538d92d9bb7a
kakani sridhar <sreedhara.kakani@yahoo.co.uk>
2021-04-01 11:56:00 +0100

    Added more tests for PUT /api/{id}/data

--- designer/server/plugins/routes/__tests__/api.jest.ts
+++ designer/server/plugins/routes/__tests__/api.jest.ts
@@ -130,4 +130,90 @@ describe("Server API", () => {
       "attachment; filename=test-form-id-crash-report"
     );
   });
+
+  test("Schema validation failures should return 401", async () => {
+    const options = {
+      method: "put",
+      url: "/api/test-form-id/data",
+      payload: {
+        metadata: {},
+        startPage: "/first-page",
+        pages: [
+          {
+            title: "First page",
+            path: "/first-page",
+            components: [],
+            next: [
+              {
+                path: "/summary",
+              },
+            ],
+          },
+          {
+            title: "Summary",
+            path: "/summary",
+            controller: "./pages/summary.js",
+            components: [],
+          },
+        ],
+        lists: [],
+        conditions: [],
+        fees: [],
+        outputs: [],
+        version: 2,
+      },
+    };
+
+    const result = await server.inject(options);
+    expect(result.statusCode).toEqual(401);
+    expect(result.result.err.message).toMatch("Schema validation failed");
+  });
+
+  test("persistence service errors should return 401", async () => {
+    //Given
+    const { persistenceService } = server.services();
+    persistenceService.uploadConfiguration = () => {
+      return Promise.reject(new Error("Error in persistence service"));
+    };
+
+    const options = {
+      method: "put",
+      url: "/api/test-form-id/data",
+      payload: {
+        metadata: {},
+        startPage: "/first-page",
+        pages: [
+          {
+            title: "First page",
+            path: "/first-page",
+            components: [],
+            next: [
+              {
+                path: "/summary",
+              },
+            ],
+          },
+          {
+            title: "Summary",
+            path: "/summary",
+            controller: "./pages/summary.js",
+            components: [],
+          },
+        ],
+        lists: [],
+        sections: [],
+        conditions: [],
+        fees: [],
+        outputs: [],
+        version: 2,
+      },
+    };
+
+    //When
+    const result = await server.inject(options);
+
+    //Then
+    expect(result.statusCode).toEqual(401);
+    expect(result.result.err.message).toEqual("Error in persistence service");
+  });
 });
