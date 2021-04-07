ffa2d8d2e08d3f9792ea23631f0b84ca6eaeec75
Max Stewart <max@cautionyourblast.com>
2021-04-08 10:36:40 +1200

    Don't include fees unless there are applicable fees.

--- runner/src/server/plugins/engine/models/submission/FeesModel.ts
+++ runner/src/server/plugins/engine/models/submission/FeesModel.ts
@@ -7,28 +7,29 @@ export function FeesModel(
   model: FormModel,
   state: FormSubmissionState
 ): Fees | undefined {
-  let applicableFees: FeeDetails[] = [];
-
   if (model.def.fees) {
-    applicableFees = model.def.fees.filter((fee) => {
+    const applicableFees: FeeDetails[] = model.def.fees.filter((fee) => {
       return !fee.condition || model.conditions[fee.condition].fn(state);
     });
 
-    const flatState = flatten(state);
+    if (applicableFees.length > 0) {
+      // @ts-ignore
+      const flatState = flatten(state);
 
-    return {
-      details: applicableFees,
-      total: Object.values(applicableFees)
-        .map((fee) => {
-          if (fee.multiplier) {
-            const multiplyBy = flatState[fee.multiplier];
-            fee.multiplyBy = Number(multiplyBy);
-            return fee.multiplyBy * fee.amount;
-          }
-          return fee.amount;
-        })
-        .reduce((a, b) => a + b, 0),
-    };
+      return {
+        details: applicableFees,
+        total: Object.values(applicableFees)
+          .map((fee) => {
+            if (fee.multiplier) {
+              const multiplyBy = flatState[fee.multiplier];
+              fee.multiplyBy = Number(multiplyBy);
+              return fee.multiplyBy * fee.amount;
+            }
+            return fee.amount;
+          })
+          .reduce((a, b) => a + b, 0),
+      };
+    }
   }
 
   return undefined;
