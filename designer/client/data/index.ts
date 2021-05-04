3b15680a9f576880ada9643ba91f05b4c20c7d70
jenbutongit <jen+git@cautionyourblast.com>
2021-05-04 18:22:57 +0100

    remove dataReducer

--- designer/client/data/index.ts
+++ designer/client/data/index.ts
@@ -1,8 +1,6 @@
 export * from "./types";
 export * from "./component";
-export * from "./list/findList";
-// export * from "./useFeedback";
-export * from "./component/inputs";
+export * from "./list";
 export * from "./page";
 export * from "./section";
 export * from "./condition";
