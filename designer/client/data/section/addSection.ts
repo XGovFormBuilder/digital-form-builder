16d8608a4a95ab7b3e21c00adeb7cdaab09448d2
jenbutongit <jen+git@cautionyourblast.com>
2021-05-04 16:08:18 +0100

    addSection tests

--- designer/client/data/section/addSection.ts
+++ designer/client/data/section/addSection.ts
@@ -4,6 +4,9 @@ export function addSection(
   data: FormDefinition,
   section: Section
 ): FormDefinition {
+  if (data.sections.find((s) => s.name === section.name)) {
+    throw Error(`A section with the name ${section.name} already exists`);
+  }
   return {
     ...data,
     sections: [...data.sections, section],
