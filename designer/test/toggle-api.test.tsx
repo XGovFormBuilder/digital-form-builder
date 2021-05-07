de980e305c59851402a9ca89b46720866b4ce19a
Enda McCormack <endamccormack0@gmail.com>
2021-05-07 07:43:06 +0100

    switched out stubs for mocks in toggle-api

--- designer/test/toggle-api.test.tsx
+++ designer/test/toggle-api.test.tsx
@@ -1,38 +1,30 @@
 import * as Code from "@hapi/code";
 import * as Lab from "@hapi/lab";
-import { stubFetchJson, restoreWindowMethods } from "./helpers/window-stubbing";
 import { FeatureToggleApi } from "../client/api/toggleApi";
+import { rest } from "msw";
 
 const { expect } = Code;
 const lab = Lab.script();
 exports.lab = lab;
-const { suite, test, afterEach } = lab;
+const { suite, test } = lab;
 
 suite("Toggle API", () => {
-  afterEach(() => {
-    restoreWindowMethods();
-  });
+  const url = "/feature-toggles";
 
   test("Should fetch feature toggles", async () => {
     const toggle = [{ ff_somevalue: "false" }];
-    stubFetchJson(200, toggle);
-
+    rest.get(url, (req, res, ctx) => {
+      return res(ctx.status(200), ctx.json(toggle));
+    });
     const result = await new FeatureToggleApi().fetch();
-
-    expect(window.fetch.callCount).to.equal(1);
-    expect(window.fetch.firstCall.args[0]).to.equal("/feature-toggles");
-
     expect(result).to.equal(result);
   });
 
   test("Should return nothing on server error", async () => {
-    stubFetchJson(500, "Some error happened");
-
+    rest.get(url, (req, res, ctx) => {
+      return res(ctx.status(500), ctx.json("Some error happened"));
+    });
     const result = await new FeatureToggleApi().fetch();
-
-    expect(window.fetch.callCount).to.equal(1);
-    expect(window.fetch.firstCall.args[0]).to.equal("/feature-toggles");
-
     expect(result).to.equal([]);
   });
 });
