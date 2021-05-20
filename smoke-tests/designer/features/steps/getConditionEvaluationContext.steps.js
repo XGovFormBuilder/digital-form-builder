d9eb10c1ff50e319cab60ae5762b99a9e02a9728
Carlos Gutierrez <carlos@spartan-testsolutions.co.uk>
2021-05-20 12:27:27 +0100

    Created steps and refacotring

--- smoke-tests/designer/features/steps/getConditionEvaluationContext.steps.js
+++ smoke-tests/designer/features/steps/getConditionEvaluationContext.steps.js
@@ -1,52 +1,66 @@
 const { Given, When, Then } = require("cucumber");
 const { formRunner } = require("../pageobjects/pages");
-const forms = require("../actions/forms");
-const { toCamelCase } = require("../../support/testHelpers");
-const formData = require("../../data/formData");
 
-Given(
-  "I complete the form as if I have a UK passport until I reach the TestConditions page",
-  function () {
-    formRunner.selectRadio("Yes");
-    formRunner.selectDropdownOption("1");
-    formRunner.continueButton.click();
-    formRunner.inputField("First name", "Applicant");
-    formRunner.inputField("Surname", "Example");
-    formRunner.continueButton.click();
-    formRunner.inputField(
-      "Address line 1",
-      "4th Floor, Block C, The Soapworks"
-    );
-    formRunner.inputField("Address line 2", "Colgate Lane");
-    formRunner.inputField("Town or city", "Manchester");
-    formRunner.inputField("Postcode", "M5 3LZ");
-    formRunner.continueButton.click();
-    formRunner.inputField("Phone number", "1234567890");
-    formRunner.inputField("Your email address", "developer@example.com");
-    formRunner.continueButton.click();
-  }
-);
-
-Given(
-  "I complete the form as if I do not have a UK passport until I reach the TestConditions page",
-  function () {
-    formRunner.selectRadio("No");
-  }
-);
-
-Then("the TestConditions page is displayed", function () {
-  expect(formRunner.pageTitle).toHaveText("TestConditions");
+Given(/^I choose "([^"]*)" for "([^"]*)"$/, function (
+  usersChoice,
+  questionText
+) {
+  browser.waitUntil(() =>
+    formRunner.pageTitle.getText().includes(questionText)
+  );
+  formRunner.selectRadio(usersChoice);
 });
 
-Then("I see a paragraph containing {string}", function (paragraphText) {
-  expect(formRunner.paragraph).toHaveText(paragraphText);
+When(/^I progress to the TestConditions page$/, function () {
+  formRunner.selectDropdownOption("1");
+  formRunner.continueButton.click();
+  formRunner.inputField("First name", "Applicant");
+  formRunner.inputField("Surname", "Example");
+  formRunner.continueButton.click();
+  formRunner.inputField("Address line 1", "4th Floor, Block C, The Soapworks");
+  formRunner.inputField("Address line 2", "Colgate Lane");
+  formRunner.inputField("Town or city", "Manchester");
+  formRunner.inputField("Postcode", "M5 3LZ");
+  formRunner.continueButton.click();
+  formRunner.inputField("Phone number", "1234567890");
+  formRunner.inputField("Your email address", "developer@example.com");
+  formRunner.continueButton.click();
 });
 
-Then("I do not see a paragraph containing {string}", function (paragraphText) {
-  expect(formRunner.paragraph).not.toExist();
+Then(/^the text "([^"]*)" "(is|is not)" displayed$/, function (
+  paragraphText,
+  textState
+) {
+  textState === "is"
+    ? expect(formRunner.paragraph).toHaveText(paragraphText)
+    : expect(formRunner.paragraph).not.toExist();
 });
 
-Then("I go back to the {string} page", function (startPage) {
+Given(/^I see the text "([^"]*)" on the TestConditions page$/, function (
+  pageText
+) {
+  let formPath = "get condition evaluation context"
+    .replace(/ /g, "-")
+    .toLowerCase();
+  formRunner.open(formPath);
+  formRunner.selectRadio("Yes");
+  formRunner.selectDropdownOption("1");
+  formRunner.continueButton.click();
+  formRunner.inputField("First name", "Applicant");
+  formRunner.inputField("Surname", "Example");
+  formRunner.continueButton.click();
+  formRunner.inputField("Address line 1", "4th Floor, Block C, The Soapworks");
+  formRunner.inputField("Address line 2", "Colgate Lane");
+  formRunner.inputField("Town or city", "Manchester");
+  formRunner.inputField("Postcode", "M5 3LZ");
+  formRunner.continueButton.click();
+  formRunner.inputField("Phone number", "1234567890");
+  formRunner.inputField("Your email address", "developer@example.com");
+  formRunner.continueButton.click();
+  browser.waitUntil(() => formRunner.paragraph.getText().includes(pageText));
+});
+
+When("I go back to the {string} page", function (startPage) {
   while (!formRunner.pageTitle.getText().includes(startPage)) {
     formRunner.backToPreviousPage.click();
   }
