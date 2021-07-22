e3aea3fabf26eedc076dc8213895b31d69f4a408
Bertrandmc <bemacode@gmail.com>
2021-07-22 12:08:49 +0100

    Make sure types are correct before strictly comparing

---
+++ runner/test/cases/server/plugins/engine/components/ListFormComponents.test.ts
@@ -0,0 +1,64 @@
+import * as Code from "@hapi/code";
+import * as Lab from "@hapi/lab";
+import { ListFormComponent } from "server/plugins/engine/components/ListFormComponent";
+import { FormSubmissionState } from "server/plugins/engine";
+const lab = Lab.script();
+exports.lab = lab;
+const { expect } = Code;
+const { suite, describe, it, beforeEach } = lab;
+import sinon from "sinon";
+
+const lists = [
+  {
+    title: "Turnaround",
+    name: "Turnaround",
+    type: "string",
+    items: [
+      { text: "1 hour", value: "1" },
+      { text: "2 hours", value: "2" },
+    ],
+  },
+];
+
+suite("ListFormComponent", () => {
+  let componentDefinition;
+  let formModel;
+  let component;
+
+  beforeEach(() => {
+    componentDefinition = {
+      subType: "field",
+      type: "ListFormComponent",
+      name: "MyListFormComponent",
+      title: "Turnaround?",
+      options: {},
+      list: "Turnaround",
+      schema: {},
+    };
+
+    formModel = {
+      getList: () => lists[0],
+      makePage: () => sinon.stub(),
+    };
+
+    component = new ListFormComponent(componentDefinition, formModel);
+  });
+
+  describe("getDisplayStringFromState", () => {
+    it("it gets value correctly when state value is string", () => {
+      const state: FormSubmissionState = {
+        progress: [],
+        MyListFormComponent: "2",
+      };
+      expect(component.getDisplayStringFromState(state)).to.equal("2");
+    });
+
+    it("it gets value correctly when state value is number", () => {
+      const state: FormSubmissionState = {
+        progress: [],
+        MyListFormComponent: 2,
+      };
+      expect(component.getDisplayStringFromState(state)).to.equal("2");
+    });
+  });
+});
