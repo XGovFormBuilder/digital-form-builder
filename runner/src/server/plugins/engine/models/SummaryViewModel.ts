8ba3f0051568874b078f0efd65160f96d1dbba83
jenbutongit <jen+git@cautionyourblast.com>
2021-09-23 18:08:54 +0100

    refactor to avoid hoisting and/or assigning outside out scope

--- runner/src/server/plugins/engine/models/SummaryViewModel.ts
+++ runner/src/server/plugins/engine/models/SummaryViewModel.ts
@@ -10,6 +10,7 @@ import { FormSubmissionState } from "../types";
 import {
   FEEDBACK_CONTEXT_ITEMS,
   Fields,
+  Question,
   Questions,
   WebhookData,
 } from "./types";
@@ -98,7 +99,7 @@ export class SummaryViewModel {
       this.processErrors(result, details);
     } else {
       this.fees = FeesModel(model, state);
-      this.parseDataForWebhook(model, relevantPages, details);
+      this.parseDataForWebhook(model, relevantPages, details, state);
       this._webhookData = this.addFeedbackSourceDataToWebhook(
         this._webhookData,
         model,
@@ -278,103 +279,13 @@ export class SummaryViewModel {
     return { relevantPages, endPage };
   }
 
-  private toEnglish(localisableString) {
-    let englishString = "";
-    if (localisableString) {
-      if (typeof localisableString === "string") {
-        englishString = localisableString;
-      } else {
-        englishString = localisableString.en;
-      }
-    }
-    return englishString;
-  }
-
   private parseDataForWebhook(model: FormModel, relevantPages, details) {
-    const questions: Questions = [];
-
-    for (const page of relevantPages) {
-      const category = page.section?.name;
-      const isRepeatable = !!page.repeatField;
-      const detail = details.find((d) => d.name === category);
-
-      let question;
-      if (page.title) {
-        question = this.toEnglish(page.title);
-      } else {
-        question = page.components.formItems
-          .map((item) => this.toEnglish(item.title))
-          .join(", ");
-      }
-
-      let items;
-      if (isRepeatable) {
-        items = detail.items;
-      } else {
-        items = [detail.items];
-      }
-
-      for (let index = 0; index < items.length; index++) {
-        const item = items[index].filter(
-          (detailItem) => detailItem.pageId === `/${model.basePath}${page.path}`
-        );
-        const fields: Fields = [];
-
-        for (const detailItem of item) {
-          const answer =
-            detailItem.dataType !== "list"
-              ? detailItem.value
-              : detailItem.rawValue;
-          fields.push({
-            key: detailItem.name,
-            title: this.toEnglish(detailItem.title),
-            type: detailItem.dataType,
-            answer,
-          });
-
-          if (detailItem.items) {
-            const selectedItem = detailItem.items.filter(
-              (i) => i.value === answer
-            )[0];
-            if (selectedItem && selectedItem.childrenCollection) {
-              selectedItem.childrenCollection.formItems.forEach((cc) => {
-                const itemDetailItem = detail.items.find(
-                  (detailItem) => detailItem.name === cc.name
-                );
-                fields.push({
-                  key: cc.name,
-                  title: this.toEnglish(cc.title),
-                  type: cc.dataType,
-                  answer:
-                    itemDetailItem.dataType !== "list"
-                      ? itemDetailItem.value
-                      : itemDetailItem.rawValue,
-                });
-              });
-            }
-          }
-        }
-
-        questions.push({
-          category,
-          question,
-          fields,
-          index,
-        });
-      }
-    }
-
-    // default name if no name is provided
-    let englishName = `${serviceName} ${model.basePath}`;
-    if (model.name) {
-      englishName = typeof model.name === "string" ? model.name : model.name.en;
-    }
-
     this._webhookData = {
       metadata: model.def.metadata,
-      name: englishName,
-      questions: questions,
+      name: model.name?.en ?? model.name ?? `${serviceName} ${model.basePath}`,
+      questions: parseDetailsToWebhookQuestion(relevantPages, details),
     };
+
     if (this.fees) {
       this._webhookData.fees = this.fees;
     }
@@ -528,4 +439,64 @@ function Item(
     title: component.title,
     dataType: component.dataType,
   };
+}
+
+function detailItemToWebhookAnswer(item) {
+  switch (item.dataType) {
+    case "list":
+    case "date":
+      return item.rawValue;
+    default:
+      return item.value;
+  }
+}
+
+function parseDetailsToWebhookQuestion(relevantPages, details) {
+  const questions = relevantPages.map((page) => {
+    const isRepeatable = !!page.repeatField;
+
+    const filteredItems = details.flatMap((detail) =>
+      detail.items.filter((item) => item.path === page.path)
+    );
+
+    const items = isRepeatable ? [filteredItems] : filteredItems;
+    let index;
+    const fields = items.flatMap((item, i) => {
+      index = i;
+      const answer = detailItemToWebhookAnswer(item);
+      const fields = [
+        { key: item.name, title: item.title, type: item.dataType, answer },
+      ];
+      if (item.items) {
+        const selectedItem = items.items.find((i) => i.value === answer);
+        const nestedFields = selectedItem.childrenCollection.formItems.map(
+          (cc) => {
+            const itemDetailItem = items.items.find(
+              (detailItem) => detailItem.name === cc.name
+            );
+            return {
+              key: cc.name,
+              title: cc.title?.en ?? cc.title,
+              type: cc.dataType,
+              answer: detailItemToWebhookAnswer(itemDetailItem),
+            };
+          }
+        );
+        fields.push(nestedFields);
+      }
+      return fields;
+    });
+
+    return {
+      category: page.section,
+      question:
+        page.title?.en ??
+        page.title ??
+        page.components.formItems.map((item) => item.title),
+      fields,
+      index,
+    };
+  });
+
+  return questions;
 }
