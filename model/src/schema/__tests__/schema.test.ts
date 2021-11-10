8a241509137d321df3643e0f4b013513c7151c95
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2021-11-10 11:31:38 +0000

    Add test for schema fix

---
+++ model/src/schema/__tests__/schema.test.ts
@@ -0,0 +1,29 @@
+// @ts-nocheck
+
+import { Schema } from "../schema";
+
+describe("Form schema", () => {
+  test("allows feedback URL to be an empty string when feedbackForm is false", () => {
+    const goodConfiguration = {
+      metadata: {},
+      startPage: "/first-page",
+      pages: [],
+      lists: [],
+      sections: [],
+      conditions: [],
+      fees: [],
+      outputs: [],
+      version: 2,
+      skipSummary: false,
+      name: "Schema fix 3",
+      feedback: { feedbackForm: false, url: "" },
+      phaseBanner: {},
+    };
+
+    const { value, error } = Schema.validate(goodConfiguration, {
+      abortEarly: false,
+    });
+
+    expect(error).toEqual(undefined);
+  });
+});
