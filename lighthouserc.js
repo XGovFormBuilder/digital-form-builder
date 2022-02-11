ba1e337a3a2025b14552a13e2acb625da107d25a
adamdavies1 <adam.davies@evoco.co.uk>
2022-02-11 15:07:51 +0000

    setting accessibility only

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,24 +1,28 @@
 module.exports = {
-    ci: {
-      collect: {
-        url: [
-          "http://localhost:3000/app",
-          "http://localhost:3000/app/new",
-          "http://localhost:3000/app/choose-existing",
-          "http://localhost:3000/app/designer/test-form-a",
-          "http://localhost:3009/components/all-components",
-        ],
-        startServerCommand: "",
-      },
-      assert: {
-        assertions: {
-          "first-contentful-paint": ["error", { minScore: 0.6 }],
-        },
-        preset: "lighthouse:recommended",
-      },
-      upload: {
-        target: "filesystem",
-        //target: "temporary-public-storage",
+  ci: {
+    collect: {
+      url: [
+        "http://localhost:3000/app",
+        "http://localhost:3000/app/new",
+        "http://localhost:3000/app/choose-existing",
+        "http://localhost:3000/app/designer/test-form-a",
+        "http://localhost:3009/components/all-components",
+      ],
+      startServerCommand: "",
+      settings: {
+        //set which categories you want to run here.
+        onlyCategories: ["accessibility"],
       },
     },
-  };
+    assert: {
+      assertions: {
+        "first-contentful-paint": ["error", { minScore: 0.6 }],
+      },
+      preset: "lighthouse:recommended",
+    },
+    upload: {
+      target: "filesystem",
+      //target: "temporary-public-storage",
+    },
+  },
+};
