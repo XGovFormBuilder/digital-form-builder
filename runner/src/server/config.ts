dd1c462580ef334fb7b494cbd076b8cfda93559a
jenbutongit <jen+git@cautionyourblast.com>
2022-02-24 13:18:35 +0000

    nodeConfig

--- runner/src/server/config.ts
+++ runner/src/server/config.ts
@@ -2,18 +2,10 @@ import dotEnv from "dotenv";
 import Joi, { CustomHelpers } from "joi";
 const nodeConfig = require("config");
 import { isUrlSecure } from "src/server/utils/url";
-import { nanoid } from "nanoid";
 
 if (process.env.NODE_ENV !== "test") {
   dotEnv.config({ path: ".env" });
 }
-
-const minute = 60 * 1000;
-const DEFAULT_SESSION_TTL = 20 * minute;
-const DEFAULT_PORT = 3009;
-const DEFAULT_LOG_LEVEL = "trace";
-const DEFAULT_SERVICE_URL = "http://localhost:3009";
-const DEFAULT_DOCUMENT_UPLOAD_API_URL = "http://localhost:9000";
 
 function secureUrl(value: string, helper: CustomHelpers) {
   if (isUrlSecure(value)) {
@@ -29,56 +21,53 @@ function secureUrl(value: string, helper: CustomHelpers) {
  * joi schema validation is used here to ensure that there are not invalid key/values when a server is starting up.
  */
 const schema = Joi.object({
-  port: Joi.number().default(DEFAULT_PORT),
+  port: Joi.number(),
   env: Joi.string().valid("development", "test", "production"),
   logLevel: Joi.string()
     .optional()
-    .default(DEFAULT_LOG_LEVEL)
     .allow("trace", "debug", "info", "warn", "error"),
   ordnanceSurveyKey: Joi.string().optional(),
   browserRefreshUrl: Joi.string().optional(),
-  feedbackLink: Joi.string().default("#"),
-  phaseTag: Joi.string().optional().valid("", "alpha", "beta").default("beta"),
+  feedbackLink: Joi.string(),
+  phaseTag: Joi.string().optional().valid("", "alpha", "beta"),
   gtmId1: Joi.string().optional(),
   gtmId2: Joi.string().optional(),
   matomoId: Joi.string().optional(),
   matomoUrl: Joi.string().custom(secureUrl).optional(),
-  payApiUrl: Joi.string()
-    .custom(secureUrl)
-    .default("https://publicapi.payments.service.gov.uk/v1"),
+  payApiUrl: Joi.string().custom(secureUrl),
   payReturnUrl: Joi.string().custom(secureUrl),
-  serviceUrl: Joi.string().optional().default(DEFAULT_SERVICE_URL),
+  serviceUrl: Joi.string().optional(),
   redisHost: Joi.string().optional(),
   redisPort: Joi.number().optional(),
   redisPassword: Joi.string().optional(),
   redisTls: Joi.boolean().optional(),
   serviceName: Joi.string().optional(),
-  documentUploadApiUrl: Joi.string().default(DEFAULT_DOCUMENT_UPLOAD_API_URL),
+  documentUploadApiUrl: Joi.string(),
   previewMode: Joi.boolean().optional(),
   sslKey: Joi.string().optional(),
   sslCert: Joi.string().optional(),
-  sessionTimeout: Joi.number().default(DEFAULT_SESSION_TTL),
+  sessionTimeout: Joi.number(),
   sessionCookiePassword: Joi.string().optional(),
   rateLimit: Joi.boolean().optional(),
-  fromEmailAddress: Joi.string().optional(),
-  serviceStartPage: Joi.string().optional(),
+  fromEmailAddress: Joi.string().optional().allow(""),
+  serviceStartPage: Joi.string().optional().allow(""),
   privacyPolicyUrl: Joi.string().optional().allow(""),
   notifyTemplateId: Joi.string()
     .when("env", {
       is: "production",
       then: Joi.required(),
-      otherwise: Joi.optional(),
+      otherwise: Joi.optional().allow(""),
     })
     .label("NOTIFY_TEMPLATE_ID"),
   notifyAPIKey: Joi.string()
     .when("env", {
       is: "production",
       then: Joi.required(),
-      otherwise: Joi.optional(),
+      otherwise: Joi.optional().allow(""),
     })
     .label("NOTIFY_API_KEY"),
-  lastCommit: Joi.string().default("undefined"),
-  lastTag: Joi.string().default("undefined"),
+  lastCommit: Joi.string(),
+  lastTag: Joi.string(),
   apiEnv: Joi.string().allow("test", "production", "").optional(),
   authEnabled: Joi.boolean().optional(),
   authClientId: Joi.string().when("authEnabled", {
@@ -101,57 +90,20 @@ const schema = Joi.object({
     then: Joi.required(),
     otherwise: Joi.optional(),
   }),
+  safelist: Joi.array().items(Joi.string()),
+  initialisedSessionTimeout: Joi.number(),
+  initialisedSessionKey: Joi.string(),
 });
 
 export function buildConfig() {
   // Build config
-  const config = {
-    port: process.env.PORT,
-    env: process.env.NODE_ENV,
-    logLevel: process.env.LOG_LEVEL,
-    ordnanceSurveyKey: process.env.ORDNANCE_SURVEY_KEY,
-    browserRefreshUrl: process.env.BROWSER_REFRESH_URL,
-    feedbackLink: process.env.FEEDBACK_LINK,
-    phaseTag: process.env.PHASE_TAG,
-    gtmId1: process.env.GTM_ID_1,
-    gtmId2: process.env.GTM_ID_2,
-    matomoId: process.env.MATOMO_ID,
-    matomoUrl: process.env.MATOMO_URL,
-    payApiUrl: process.env.PAY_API_URL,
-    payReturnUrl: process.env.PAY_RETURN_URL,
-    serviceUrl: process.env.SERVICE_URL,
-    redisHost: process.env.REDIS_HOST,
-    redisPort: process.env.REDIS_PORT,
-    redisPassword: process.env.REDIS_PASSWORD,
-    redisTls: process.env.REDIS_TLS === "true",
-    serviceName: process.env.SERVICE_NAME,
-    documentUploadApiUrl: process.env.DOCUMENT_UPLOAD_API_URL,
-    previewMode: process.env.PREVIEW_MODE === "true",
-    sslKey: process.env.SSL_KEY,
-    sslCert: process.env.SSL_CERT,
-    sessionTimeout: process.env.SESSION_TIMEOUT,
-    sessionCookiePassword: process.env.SESSION_COOKIE_PASSWORD,
-    rateLimit: process.env.RATE_LIMIT !== "false",
-    fromEmailAddress: process.env.FROM_EMAIL_ADDRESS,
-    serviceStartPage: process.env.SERVICE_START_PAGE,
-    privacyPolicyUrl: process.env.PRIVACY_POLICY_URL,
-    notifyTemplateId: process.env.NOTIFY_TEMPLATE_ID,
-    notifyAPIKey: process.env.NOTIFY_API_KEY,
-    lastCommit: process.env.LAST_COMMIT || process.env.LAST_COMMIT_GH,
-    lastTag: process.env.LAST_TAG || process.env.LAST_TAG_GH,
-    apiEnv: process.env.API_ENV,
-    authEnabled: process.env.AUTH_ENABLED,
-    authClientId: process.env.AUTH_CLIENT_ID,
-    authClientSecret: process.env.AUTH_CLIENT_SECRET,
-    authClientAuthUrl: process.env.AUTH_CLIENT_AUTH_URL,
-    authClientTokenUrl: process.env.AUTH_CLIENT_TOKEN_URL,
-    authClientProfileUrl: process.env.AUTH_CLIENT_PROFILE_URL,
-  };
+  const config = nodeConfig;
 
   // Validate config
   const result = schema.validate(config, {
     abortEarly: false,
     convert: true,
+    allowUnknown: true,
   });
 
   // Throw if config is invalid
@@ -159,24 +111,9 @@ export function buildConfig() {
     throw new Error(`The server config is invalid. ${result.error.message}`);
   }
 
-  // Use the Joi validated value
-  const value = result.value;
-
-  value.isProd = value.env === "production";
-  value.isDev = !value.isProd;
-  value.isTest = value.env === "test";
-  value.isSandbox = process.env.sandbox === "true"; // for heroku instances
-
-  return value;
+  return config;
 }
 
 const config = buildConfig();
-// TODO: migrate all environment variables to runner/config/default.json
-config.safelist = nodeConfig.get("safelist");
-config.initialisedSessionTimeout = nodeConfig.get("initialisedSessionTimeout"); //default 28 days
-if (!nodeConfig.has("initialisedSessionKey")) {
-  config.initialisedSessionKey = nanoid(16);
-} else {
-  config.initialisedSessionKey = nodeConfig.get("initialisedSessionKey");
-}
+
 export default config;
