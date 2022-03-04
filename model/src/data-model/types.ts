2e5449a71b84d94f33251a66e41f397d7be9e3dd
Jen Duong <jen+git@cautionyourblast.com>
2022-03-04 15:07:05 +0000

    feature: initialise session (#760)

--- model/src/data-model/types.ts
+++ model/src/data-model/types.ts
@@ -91,7 +91,7 @@ export type Output = {
 
 type Toggleable<T> = boolean | T;
 
-type ConfirmationPage = {
+export type ConfirmationPage = {
   customText: {
     title: string;
     paymentSkipped: Toggleable<string>;
---
+++ runner/src/server/schemas/types.ts
@@ -0,0 +1,38 @@
+import { ComponentType, ConfirmationPage } from "@xgovformbuilder/model";
+import { FeeDetails } from "server/services/payService";
+
+export type Field = {
+  key: string;
+  type: ComponentType;
+  title: string;
+  answer: any;
+};
+
+export type Question = {
+  category?: string;
+  question: string;
+  fields: Field[];
+};
+
+export type WebhookSchema = {
+  name: string;
+  preferredLanguage?: string;
+  fees: FeeDetails;
+  questions: Question[];
+  metadata?: { [key: string]: unknown };
+};
+
+export type InitialiseSessionField = Pick<Field, "key" | "answer">;
+export type InitialiseSessionQuestion = {
+  fields: InitialiseSessionField[];
+} & Question;
+
+export type InitialiseSessionSchema = {
+  options: {
+    callbackUrl: string;
+    redirectPath?: string;
+    customText: ConfirmationPage["customText"];
+    components: ConfirmationPage["components"];
+  };
+  questions: InitialiseSessionQuestion[];
+} & Pick<WebhookSchema, "questions" | "metadata">;
