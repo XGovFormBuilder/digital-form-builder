9bc7c3f0db08fb68a2633b2db2ffa7f57ff57b30
adamdavies1 <adam.davies@evoco.co.uk>
2022-03-10 16:28:29 +0000

    fixing pipleline

--- smoke-tests/designer/wdio.conf.js
+++ smoke-tests/designer/wdio.conf.js
@@ -1,6 +1,6 @@
 const { hooks } = require("./support/hooks");
 const drivers = {
-  chrome: { version: "97.0.4692.71" }, // https://chromedriver.chromium.org/
+  chrome: { version: "99.0.4844.51" }, // https://chromedriver.chromium.org/
   firefox: { version: "0.29.1" }, // https://github.com/mozilla/geckodriver/releases
 };
 
