5d45b2b6f4d4dd1bfdf501379dd00c469f349c1d
adamdavies1 <adam.davies@evoco.co.uk>
2022-04-08 14:18:30 +0100

    trying to fix chromedriver

--- smoke-tests/designer/wdio.conf.js
+++ smoke-tests/designer/wdio.conf.js
@@ -1,6 +1,6 @@
 const { hooks } = require("./support/hooks");
 const drivers = {
-  chrome: { version: "98.0.4758.102" }, // https://chromedriver.chromium.org/
+  chrome: { version: "100.0.4896.75" }, // https://chromedriver.chromium.org/
   firefox: { version: "0.29.1" }, // https://github.com/mozilla/geckodriver/releases
 };
 
