f2834800c0f7a682e661e93327469c68c34ed49e
Adam Davies <97108643+adamdavies1@users.noreply.github.com>
2022-04-26 17:23:49 +0100

    parent c2cabf5b3b7f6967cef42ebeb5cb2a967cbbe2ed author Adam Davies <97108643+adamdavies1@users.noreply.github.com> 1650990229 +0100 committer JamesBoadi <39907481+JamesBoadi@users.noreply.github.com> 1653472480 +0100
    parent c2cabf5b3b7f6967cef42ebeb5cb2a967cbbe2ed
    author Adam Davies <97108643+adamdavies1@users.noreply.github.com> 1650990229 +0100
    committer JamesBoadi <39907481+JamesBoadi@users.noreply.github.com> 1653472469 +0100
    
    parent c2cabf5b3b7f6967cef42ebeb5cb2a967cbbe2ed
    author Adam Davies <97108643+adamdavies1@users.noreply.github.com> 1650990229 +0100
    committer JamesBoadi <39907481+JamesBoadi@users.noreply.github.com> 1653472450 +0100
    
    parent c2cabf5b3b7f6967cef42ebeb5cb2a967cbbe2ed
    author Adam Davies <97108643+adamdavies1@users.noreply.github.com> 1650990229 +0100
    committer JamesBoadi <39907481+JamesBoadi@users.noreply.github.com> 1653472437 +0100
    
    parent c2cabf5b3b7f6967cef42ebeb5cb2a967cbbe2ed
    author Adam Davies <97108643+adamdavies1@users.noreply.github.com> 1650990229 +0100
    committer JamesBoadi <39907481+JamesBoadi@users.noreply.github.com> 1653472421 +0100
    
    parent c2cabf5b3b7f6967cef42ebeb5cb2a967cbbe2ed
    author Adam Davies <97108643+adamdavies1@users.noreply.github.com> 1650990229 +0100
    committer JamesBoadi <39907481+JamesBoadi@users.noreply.github.com> 1653472408 +0100
    
    * Whole box dragable
    
    * removed unsued code, decreased delay
    
    * Fixing chrome driver issue

--- designer/client/button-dropdown.js
+++ designer/client/button-dropdown.js
@@ -2,7 +2,7 @@ import React from "react";
 import { withI18n } from "./i18n";
 
 class ButtonDropdown extends React.Component {
-  /* TODO:- generalise this */
+  /* TODO:- generalise thi */
   constructor(props) {
     super(props);
     this.state = {
