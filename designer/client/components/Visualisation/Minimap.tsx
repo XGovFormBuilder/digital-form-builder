2094a7b7094cd659352c0064f6e6f007942e5523
adamdavies1 <adam.davies@evoco.co.uk>
2022-05-09 14:21:35 +0100

    now click off removes highlight

--- designer/client/components/Visualisation/Minimap.tsx
+++ designer/client/components/Visualisation/Minimap.tsx
@@ -6,9 +6,21 @@ type Props = {
   scale?: number;
 };
 
-function onMapClick() {
-  let test = "";
-  window.scrollTo(500, 0);
+function onMapClick(node, nodes) {
+  setCssBack(nodes);
+  window.scrollTo(
+    node.node.x - node.node.width / 2,
+    node.node.y - node.node.height / 2
+  );
+  let page = document.getElementById(node.node.label);
+  page.setAttribute("class", "page-hover");
+}
+
+function setCssBack(nodes) {
+  nodes.map((node) => {
+    let page = document.getElementById(node.node.label);
+    page.setAttribute("class", "page");
+  });
 }
 
 export const Minimap = ({ layout, data, scale = 0.05 }: Props) => (
@@ -30,8 +42,11 @@ export const Minimap = ({ layout, data, scale = 0.05 }: Props) => (
 
       {layout.nodes.map((node, index) => {
         return (
-          <g key={node + index}>
-            <a id={node + index} onClick={onMapClick}>
+          <g key={node.node.label + index}>
+            <a
+              id={node.node.label + index}
+              onClick={() => onMapClick(node, layout.nodes)}
+            >
               <rect
                 x={parseFloat(node.left) * scale}
                 y={parseFloat(node.top) * scale}
