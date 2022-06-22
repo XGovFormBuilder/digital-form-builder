5b650f2cf1ddfe6fd4e66fac9b7201ea3a26deaa
Ram <80714392+ramsharma-prog@users.noreply.github.com>
2022-06-22 09:49:46 +0100

    Update wdio.conf.js
    Chrome driver updated

--- smoke-tests/designer/wdio.conf.js
+++ smoke-tests/designer/wdio.conf.js
@@ -1,6 +1,6 @@
 const { hooks } = require("./support/hooks");
 const drivers = {
-  chrome: { version: "101.0.4951.54" }, // https://chromedriver.chromium.org/
+  chrome: { version: "102.0.5005.115" }, // https://chromedriver.chromium.org/
   firefox: { version: "0.29.1" }, // https://github.com/mozilla/geckodriver/releases
 };
 
