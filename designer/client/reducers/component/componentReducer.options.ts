2062367dd19cb4074ffb8a0092b897419364a3f8
adamdavies1 <adam.davies@evoco.co.uk>
2022-07-14 10:05:38 +0100

    fixing lint issues designer

--- designer/client/reducers/component/componentReducer.options.ts
+++ designer/client/reducers/component/componentReducer.options.ts
@@ -125,7 +125,7 @@ export function optionsReducer(state, action: OptionsActions) {
         selectedComponent: {
           ...selectedComponent,
           options: { ...options, omitFromSummary: payload },
-           },
+        },
       };
 
     case Options.EDIT_OPTIONS_MAX_WORDS:
