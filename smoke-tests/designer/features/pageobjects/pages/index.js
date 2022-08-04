961573a12dc50b4b13fe0b92db0e9ed45c59f8d3
jenbutongit <jen+git@cautionyourblast.com>
2022-08-04 17:20:32 +0100

    re-yarning

--- smoke-tests/designer/features/pageobjects/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as configPage } from "./config.page";
-export { default as formDesigner } from "./formDesigner.page";
-export { default as formRunner } from "./formRunner.page";
-export { default as previewPage } from "./preview.page";
