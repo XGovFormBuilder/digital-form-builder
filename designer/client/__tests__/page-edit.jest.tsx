c51e74aed73ff9f6741e0bb0eccd8e07533f0830
Jen Duong <jen+git@cautionyourblast.com>
2022-08-18 17:23:03 +0100

    feat: update to node 16 (#889)

---
+++ designer/client/__tests__/page-edit.jest.tsx
@@ -0,0 +1,6 @@
+test.todo("Renders a form with the correct initial inputs");
+test.todo(
+  "updating the title changes the path if the path is the auto-generated one"
+);
+test.todo("changing the section causes the new section to be selected");
+test.todo("changing the controller causes the new controller to be selected");
--- smoke-tests/designer/support/componentMapping.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  date: "DateField",
-  dateTime: "DateTimeField",
-  emailAddress: "EmailAddressField",
-  dateParts: "DatePartsField",
-  text: "TextField",
-  list: "List",
-  paragraph: "Para",
-};
