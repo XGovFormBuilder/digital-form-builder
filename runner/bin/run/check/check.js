c1d1ecea9414520daa4cbeca6fc14e9d18864c6d
Jen Duong <jen+git@cautionyourblast.com>
2022-10-18 15:54:02 +0100

    fix: resolve yarn npm audit -AR issues (#948)

--- runner/bin/run/check/check.js
+++ runner/bin/run/check/check.js
@@ -1,21 +1,20 @@
 #!/usr/bin/nodejs
 const helper = require("./getOutOfDateForms");
-const cliux = require("cli-ux");
-const { cli } = cliux;
+const { CliUx } = require("@oclif/core");
 async function check() {
-  cli.action.start("Checking versions of forms in runner/src/forms");
+  CliUx.ux.action.start("Checking versions of forms in runner/src/forms");
   const files = helper.getOutOfDateForms();
-  cli.action.stop();
+  CliUx.ux.action.stop();
 
   if (files.length <= 0) {
-    cli.warn(
+    CliUx.ux.warn(
       `Your form(s) ${files.join(
         ", "
       )} is/are out of date. Use the designer to upload your files, which runs the migration scripts. Download those JSONs to replace the outdated forms. Migration scripts will not cover conditional reveal fields. You will need to fix those manually.`
     );
     process.exit(1);
   } else {
-    cli.info("Your forms are up to date");
+    CliUx.ux.info("Your forms are up to date");
   }
 }
 
