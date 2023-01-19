b1bb688b2458b6d1c41f76d2899772e0c3baa401
adamdavies1 <adam.davies@evoco.co.uk>
2023-01-19 17:02:14 +0000

    chrome driver upgrade for smoke tests

--- smoke-tests/designer/wdio.conf.js
+++ smoke-tests/designer/wdio.conf.js
@@ -1,6 +1,6 @@
 const { hooks } = require("./support/hooks");
 const drivers = {
-  chrome: { version: "106.0.5249.61" }, // https://chromedriver.chromium.org/
+  chrome: { version: "109.0.5414.74" }, // https://chromedriver.chromium.org/
   firefox: { version: "0.29.1" }, // https://github.com/mozilla/geckodriver/releases
 };
 
