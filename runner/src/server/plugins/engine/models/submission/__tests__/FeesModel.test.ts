f62b2d1afdd6c548c644e5f20a4712b77ec1f74c
Luke Zigler <luke@cautionyourblast.com>
2023-05-15 14:52:36 +0100

    Updated fees model tests to include new tests for the multiplier functionality

--- runner/src/server/plugins/engine/models/submission/__tests__/FeesModel.test.ts
+++ runner/src/server/plugins/engine/models/submission/__tests__/FeesModel.test.ts
@@ -8,7 +8,7 @@ const { suite, test } = lab;
 import json from "./FeesModel.test.json";
 import { FormModel } from "server/plugins/engine/models";
 
-suite("FeesModel", () => {
+suite.only("FeesModel", () => {
   test("returns correct FeesModel", () => {
     const c = {
       caz: "2",
@@ -18,12 +18,42 @@ suite("FeesModel", () => {
     const model = FeesModel(form, c);
     expect(model).to.equal({
       details: [
-        { description: "Bristol tax", amount: 5000, condition: "dFQTyf" },
-        { description: "car tax", amount: 5000 },
+        {
+          description: "Bristol tax",
+          amount: 5000,
+          condition: "dFQTyf",
+          multiplier: "numberOfCars",
+        },
+        {
+          description: "car tax",
+          amount: 5000,
+          multiplier: "2",
+          multiplyBy: 2,
+        },
       ],
-      total: 10000,
+      total: 15000,
       prefixes: [],
       referenceFormat: "FCDO-{{DATE}}",
     });
+  });
+  test("returns correct total for a fee with a static quantity", () => {
+    const c = {
+      caz: "1",
+      numberOfCars: "4",
+    };
+
+    const form = new FormModel(json, {});
+    const model = FeesModel(form, c);
+    expect(model?.total).to.equal(10000);
+  });
+  test("returns correct total for a fee with a dynamic quantity", () => {
+    const c = {
+      caz: "2",
+      numberOfCars: "4",
+    };
+
+    const form = new FormModel(json, {});
+    const model = FeesModel(form, c);
+    expect(model?.total).to.equal(30000);
   });
 });
