e457d660a9261c49720f346a68a8954e761751aa
beatriz-goncalves-rocha <beatriz.rocha@test-and-trace.nhs.uk>
2023-06-26 22:00:26 +0100

    test redis

--- runner/config/default.js
+++ runner/config/default.js
@@ -69,10 +69,10 @@ module.exports = {
   confirmationSessionTimeout: 20 * minute,
   paymentSessionTimeout: 90 * minute, // GOV.UK Pay sessions are 90 minutes. It is possible a user takes longer than 20 minutes to complete a payment.
   //sessionCookiePassword: "",
-  redisHost: "redis-dev-uks-replication-group-001.redis-dev-uks-replication-group.pk2rwg.euw2.cache.amazonaws.com",
+  redisHost: "http://localhost",
   redisPort: 6379,
   //redisPassword: nanoid.random(16), // This should be set if you are deploying replicas
-  redisTls: true, //run in TLS mode
+  //redisTls: true, //run in TLS mode
 
   /**
    * SSL
