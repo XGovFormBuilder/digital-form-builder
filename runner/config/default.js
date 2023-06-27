512e2bf58c951b17ce2ca7dad3100ca88b62f48b
beatriz-goncalves-rocha <beatriz.rocha@test-and-trace.nhs.uk>
2023-06-27 11:14:56 +0100

    revert

--- runner/config/default.js
+++ runner/config/default.js
@@ -69,10 +69,10 @@ module.exports = {
   confirmationSessionTimeout: 20 * minute,
   paymentSessionTimeout: 90 * minute, // GOV.UK Pay sessions are 90 minutes. It is possible a user takes longer than 20 minutes to complete a payment.
   //sessionCookiePassword: "",
-  //redisHost: "https://test-redis.pk2rwg.clustercfg.euw2.cache.amazonaws.com",
-  //redisPort: 6379,
+  redisHost: "https://test-redis.pk2rwg.clustercfg.euw2.cache.amazonaws.com",
+  redisPort: 6379,
   //redisPassword: "", // This should be set if you are deploying replicas
-  //redisTls: true, //run in TLS mode
+  redisTls: true, //run in TLS mode
 
   /**
    * SSL
