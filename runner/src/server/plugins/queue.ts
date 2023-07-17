14c07deae82627fa2882f975e354d33b1dace61d
jen <jen@cautionyourblast.com>
2023-07-17 19:49:09 +0100

    exit code, logs

--- runner/src/server/plugins/queue.ts
+++ runner/src/server/plugins/queue.ts
@@ -1,16 +1,34 @@
 import config from "server/config";
 import { spawnSync } from "child_process";
-import * as queueModel from "@xgovformbuilder/queueModel";
-
+import { SCHEMA_LOCATION } from "@xgovformbuilder/queueModel";
 const DEFAULT_OPTIONS = {
   enableQueueService: config.enableQueueService,
 };
 export const queue = {
   name: "queue",
   register: async function (server, options) {
-    const process = spawnSync("prisma", ["migrate", "deploy"], {
-      encoding: "utf-8",
-    });
-    console.log(process);
+    console.log(SCHEMA_LOCATION);
+    const schema = require.resolve("@xgovformbuilder/queueModel/schema.prisma");
+
+    const child = spawnSync(
+      "prisma",
+      ["migrate", "deploy", "--schema", schema],
+      {
+        encoding: "utf-8",
+        stdio: "inherit",
+      }
+    );
+
+    if (child.error) {
+      server.error(["queue initialisation"], child.error);
+      process.exit(1);
+    }
+    if (child.stdout) {
+      server.info(child.stdout);
+      server.info(
+        ["queue initialisation"],
+        "Database migration was successful, continuing"
+      );
+    }
   },
 };
