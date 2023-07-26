b9b15c06c543be5367819991233a1548350ecf06
Luke Zigler <luke@cautionyourblast.com>
2023-07-26 10:15:24 +0100

    Renamed queue plugin to match rest of plugins

--- runner/src/server/plugins/queue.ts
+++ runner/src/server/plugins/queue.ts
@@ -3,31 +3,38 @@ import { spawnSync } from "child_process";
 const DEFAULT_OPTIONS = {
   enableQueueService: config.enableQueueService,
 };
-export const queue = {
+export const pluginQueue = {
   name: "queue",
   register: async function (server, options) {
-    const schemaLocation = require.resolve(
-      "@xgovformbuilder/queueModel/schema.prisma"
-    );
+    if (DEFAULT_OPTIONS.enableQueueService) {
+      const schemaLocation = require.resolve(
+        "@xgovformbuilder/queueModel/schema.prisma"
+      );
 
-    const child = spawnSync(
-      "prisma",
-      ["migrate", "deploy", "--schema", schemaLocation],
-      {
-        encoding: "utf-8",
-        stdio: "inherit",
+      const child = spawnSync(
+        "prisma",
+        ["migrate", "deploy", "--schema", schemaLocation],
+        {
+          encoding: "utf-8",
+          stdio: "inherit",
+        }
+      );
+
+      if (child.error) {
+        server.log(["error", "queue initialisation"], child.error);
+        process.exit(1);
       }
-    );
-
-    if (child.error) {
-      server.error(["queue initialisation"], child.error);
-      process.exit(1);
-    }
-    if (child.stdout) {
-      server.info(child.stdout);
-      server.info(
+      if (child.stdout) {
+        server.log(["queue initialisation", "child process"], child.stdout);
+        server.log(
+          ["queue initialisation"],
+          "Database migration was successful, continuing"
+        );
+      }
+    } else {
+      server.log(
         ["queue initialisation"],
-        "Database migration was successful, continuing"
+        "Queue service not enabled, skipping initialisation"
       );
     }
   },
