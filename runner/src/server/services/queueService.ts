63941c6f29381de470cf8f4123f6aa7ac75570c7
Luke Zigler <luke@cautionyourblast.com>
2023-07-26 10:14:48 +0100

    Updated queueService so that it pushes to db properly using prisma

--- runner/src/server/services/queueService.ts
+++ runner/src/server/services/queueService.ts
@@ -17,34 +17,22 @@ export class QueueService {
    * @param error
    * @returns The ID of the newly added row, or undefined in the event of an error
    */
-  async sendToQueue(data: object, url?: string, error?: any) {
+  async sendToQueue(data: object, url?: string) {
     const rowData = {
       data: JSON.stringify(data),
       time: new Date().getTime(),
-      webhookUrl: url ?? "",
-      error: JSON.stringify(error ?? {}),
-      retry: 0,
+      webhook_url: url ?? "",
     };
-
-    try {
-      await this.prisma.$connect();
-      const row = await this.prisma.submission.create({
-        data: rowData,
-      });
-      this.logger.log(
-        ["queueService", "sendToQueue"],
-        `Submission sent to queue successfully. Row: ${row}`
-      );
-      await this.prisma.$disconnect();
-      return row.id;
-    } catch (err) {
-      if (err instanceof Prisma.PrismaClientInitializationError) {
-        this.logger.error(["queueService", "sendToQueue"], err);
-      }
-      if (err instanceof Prisma.PrismaClientKnownRequestError) {
-        this.logger.error(["queueService", "sendToQueue"], err);
-      }
-      return;
-    }
+    await this.prisma.$connect();
+    const row = await this.prisma.submission.create({
+      data: rowData,
+    });
+    const serialisedRow = {
+      ...row,
+      time: row.time.toString(),
+    };
+    this.logger.info(["queueService", "sendToQueue", "success"], serialisedRow);
+    await this.prisma.$disconnect();
+    return serialisedRow.id;
   }
 }
