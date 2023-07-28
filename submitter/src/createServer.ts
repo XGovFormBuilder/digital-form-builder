a2a91b16dd6f8c28a49847476381d228963e943f
Luke Zigler <luke@cautionyourblast.com>
2023-07-28 11:32:23 +0100

    Implemented queue plugin to submitter service

--- submitter/src/createServer.ts
+++ submitter/src/createServer.ts
@@ -1,9 +1,11 @@
 import hapi, { ServerOptions } from "@hapi/hapi";
 import { pluginLogging } from "./plugins/logging";
+import { pluginQueue } from "./plugins/queue";
+import config from "./config";
 
 const serverOptions: ServerOptions = {
-  debug: { request: [`true`] },
-  port: 9000,
+  debug: { request: [`${config.isDev}`] },
+  port: config.port,
   router: {
     stripTrailingSlash: true,
   },
@@ -30,5 +32,6 @@ export async function createServer(): Promise<hapi.Server> {
   const server = hapi.server(serverOptions);
 
   await server.register(pluginLogging);
+  await server.register(pluginQueue);
   return server;
 }
