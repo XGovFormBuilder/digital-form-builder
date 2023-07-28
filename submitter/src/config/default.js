c70164a2128938bf92900514451bc7daa34bc6fb
Luke Zigler <luke@cautionyourblast.com>
2023-07-28 11:15:11 +0100

    Implemented some basic config for submitter service

---
+++ submitter/src/config/default.js
@@ -0,0 +1,26 @@
+const { deferConfig } = require("config/defer");
+
+module.exports = {
+  env: "development",
+  port: "9000",
+
+  /**
+   * logging config
+   */
+  logPrettyPrint: true,
+  logLevel: "info",
+
+  /**
+   * Helper flags
+   */
+  isDev: deferConfig(function () {
+    return this.env !== "production";
+  }),
+
+  /**
+   * Queue service config
+   */
+  queueDatabaseUrl: "",
+  queueDatabaseUsername: "",
+  queueDatabasePassword: "",
+};
