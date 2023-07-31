76c0a0db8c3f22ed0a3da80fb735760aa5a75232
Luke Zigler <luke@cautionyourblast.com>
2023-07-31 11:11:05 +0100

    Added in http service and webhook service

---
+++ submitter/src/services/webhookService.ts
@@ -0,0 +1,57 @@
+import { post, put } from "./httpService";
+import { HapiServer } from "../types";
+
+const DEFAULT_OPTIONS = {
+  headers: {
+    accept: "application/json",
+    "content-type": "application/json",
+  },
+  timeout: 60000,
+};
+
+export class WebhookService {
+  logger: any;
+  constructor(server: HapiServer) {
+    this.logger = server.logger;
+  }
+
+  /**
+   * Posts data to a webhook
+   * @param url - url of the webhook
+   * @param data - object to send to the webhook
+   * @param method - POST or PUT request, defaults to POST
+   * @param ref - the reference being used in the submission
+   * @returns object with the property `reference` webhook if the response returns with a reference number. If the call fails, the reference will be 'UNKNOWN'.
+   */
+  async postRequest(
+    url: string,
+    data: object,
+    method: "POST" | "PUT" = "POST",
+    ref: string
+  ) {
+    this.logger.info(
+      ["WebhookService", "postRequest body"],
+      JSON.stringify(data)
+    );
+    let request = method === "POST" ? post : put;
+    try {
+      const { payload } = await request(url, {
+        ...DEFAULT_OPTIONS,
+        payload: JSON.stringify(data),
+      });
+
+      this.logger.info(
+        ["WebhookService", "postRequest"],
+        `Webhook request to ${url} submitted OK`
+      );
+      this.logger.debug(
+        ["WebhookService", "postRequest", `REF: ${ref}`],
+        JSON.stringify(payload)
+      );
+      return;
+    } catch (error) {
+      this.logger.error(["WebhookService", "postRequest"], error);
+      return error as Error;
+    }
+  }
+}
