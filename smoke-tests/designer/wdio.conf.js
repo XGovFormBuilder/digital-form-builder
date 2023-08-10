41c815a3994ee8ed3463e90be070227cbce7a744
gio-karunakaran <gio.karunakaran@version1.com>
2023-08-10 16:25:53 +0100

    cof-r3w2-config

--- smoke-tests/designer/wdio.conf.js
+++ smoke-tests/designer/wdio.conf.js
@@ -1,6 +1,6 @@
 const { hooks } = require("./support/hooks");
 const drivers = {
-  chrome: { version: "113.0.5672.63" }, // https://chromedriver.chromium.org/
+  chrome: { version: "115.0.5790.170" }, // https://chromedriver.chromium.org/
   firefox: { version: "0.29.1" }, // https://github.com/mozilla/geckodriver/releases
 };
 
