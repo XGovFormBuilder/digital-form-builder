2582d522bf9a36649082c9c07f508f70dab690c7
Luke Zigler <luke@cautionyourblast.com>
2023-08-30 18:17:35 +0100

    Added test config to submitter service

---
+++ submitter/jest.config.js
@@ -0,0 +1,8 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+
+module.exports = {
+  testMatch: ["**/tests/**.test.ts"],
+};
