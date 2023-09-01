895e759a1911a386bf2a2898c0690b6bae3ed562
Luke Zigler <luke@cautionyourblast.com>
2023-09-01 13:12:17 +0100

    Added queueRetention workspace

---
+++ queueRetention/src/index.ts
@@ -0,0 +1,32 @@
+import config from "./config";
+import { prisma } from "./prismaClient";
+
+export async function redactSubmissions() {
+  const retentionAsTimestamp =
+    parseInt(config.retentionPeriod) * 24 * 60 * 60 * 1000;
+  const retentionLimit = new Date().getTime() - retentionAsTimestamp;
+  const validSubs = await prisma.submission.findMany({
+    where: {
+      updated_at: {
+        lt: retentionLimit,
+      },
+      complete: true,
+    },
+    select: {
+      id: true,
+    },
+  });
+  if (validSubs.length > 0) {
+    prisma.submission.deleteMany({
+      where: {
+        id: {
+          in: validSubs.map((sub) => sub.id),
+        },
+      },
+    });
+  }
+}
+
+(async () => {
+  await redactSubmissions();
+})();
