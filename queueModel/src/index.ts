4aa00d42128b70b30662ed69c8d7619ad3173121
Luke Zigler <luke@cautionyourblast.com>
2023-09-07 14:18:16 +0100

    Now exporting submission type from Prisma

--- queueModel/src/index.ts
+++ queueModel/src/index.ts
@@ -1,4 +1,4 @@
 import * as path from "path";
 
-export { PrismaClient, Prisma } from "@prisma/client";
+export { PrismaClient, Prisma, Submission } from "@prisma/client";
 export const SCHEMA_LOCATION = path.resolve(__dirname, "schema.prisma");
