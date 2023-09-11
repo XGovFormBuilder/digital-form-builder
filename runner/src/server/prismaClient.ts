84fcdf4474acf03f45fbbdfd11f5783432bf41de
Luke Zigler <luke@cautionyourblast.com>
2023-09-11 10:51:54 +0100

    Added pino back into runner and added top level prisma client

---
+++ runner/src/server/prismaClient.ts
@@ -0,0 +1,64 @@
+import type { Prisma } from "@xgovformbuilder/queueModel";
+import { PrismaClient } from "@xgovformbuilder/queueModel";
+import config from "./config";
+import logger from "pino";
+
+const prismaLogger = logger();
+
+const logLevel: Prisma.LogDefinition[] = [
+  {
+    emit: "event",
+    level: "error",
+  },
+  {
+    emit: "event",
+    level: "warn",
+  },
+];
+
+if (config.isDev) {
+  logLevel.push(
+    {
+      emit: "event",
+      level: "query",
+    },
+    {
+      emit: "event",
+      level: "info",
+    }
+  );
+}
+
+export const prisma: PrismaClient = new PrismaClient({
+  log: logLevel,
+});
+
+prisma.$connect().catch((error) => {
+  prismaLogger.error(`Prisma Connect Error ${error.message}`);
+});
+
+process.on("query", (e: Prisma.QueryEvent) => {
+  if (!config.isTest) {
+    prismaLogger.info(`
+      Prisma Query: ${e.query} \r\n
+      Duration: ${e.duration}ms \r\n
+      Params: ${e.params}
+    `);
+  }
+});
+
+process.on("warn", (e) => {
+  prismaLogger.warn(e);
+});
+
+process.on("info", (e) => {
+  prismaLogger.info(e);
+});
+
+process.on("error", (e) => {
+  prismaLogger.error(e);
+});
+
+process.on("beforeExit", () => {
+  prismaLogger.info("Prisma is exiting");
+});
