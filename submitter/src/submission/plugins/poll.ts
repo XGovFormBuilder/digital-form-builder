73b15827471920ddfd1d57e7ac24d5d4b12b5796
jen <jen@cautionyourblast.com>
2023-09-13 22:46:11 +0100

    config

--- submitter/src/submission/plugins/poll.ts
+++ submitter/src/submission/plugins/poll.ts
@@ -1,12 +1,11 @@
 import config from "../../config";
 
-console.log(config.pollingInterval);
 export const pluginPoll = {
   name: "poll",
   register: async function (server, _options) {
     const { queueService } = server.services([]);
     setInterval(async () => {
       await queueService.processSubmissions();
-    }, 5000);
+    }, config.pollingInterval);
   },
 };
