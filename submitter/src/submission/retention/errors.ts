aeb6b30182ae740aa83320b516f2c7c94defe32d
jen <jen@cautionyourblast.com>
2023-09-14 13:20:34 +0100

    tests

--- submitter/src/submission/retention/errors.ts
+++ submitter/src/submission/retention/errors.ts
@@ -1,4 +1,4 @@
-const ERRORS = {
+export const R_ERRORS = {
   CONFIG: `R001 - Could not set retention period`,
   DELETION_FAILED: `R002 - Could not delete records`,
   RUN_ERROR: `R003 - Could not run, caught exception`,
