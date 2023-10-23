18cc63c1d70b9c826ff5c3d3eba9329d380c5e1f
Luke Zigler <luke@cautionyourblast.com>
2023-10-23 12:59:55 +0100

    Updated poll plugin for better logging

--- submitter/src/submission/plugins/poll.ts
+++ submitter/src/submission/plugins/poll.ts
@@ -1,24 +1,28 @@
 import config from "../../config";
 import { QueueService } from "../services";
+import { Logger } from "pino";
 
 export const pluginPoll = {
   name: "poll",
   register: async function (server, _options) {
     const { queueService } = server.services([]);
-    await poll(queueService);
+    await poll(queueService, server.logger);
   },
 };
 
-async function poll(queueService: QueueService) {
+async function poll(queueService: QueueService, logger: Logger) {
   const submission = await queueService.getSubmissions();
   if (!submission) {
-    setTimeout(() => {
-      poll(queueService);
+    logger.info(["poll"], "No unprocessed submissions found. Continuing");
+    setTimeout(async () => {
+      await poll(queueService, logger);
     }, config.pollingInterval);
   } else {
+    logger.info(
+      ["poll"],
+      `Unprocessed submission found. Row ref: ${submission.id}`
+    );
     await queueService.submit(submission);
-    setTimeout(() => {
-      poll(queueService);
-    }, config.pollingInterval);
+    await poll(queueService, logger);
   }
 }
