1506b0bef7bb4fd2a40917f8028e7dd31e498dbc
Luke Zigler <luke@cautionyourblast.com>
2023-11-07 20:05:37 +0000

    Updated schema and migration to default allow_retry to true

--- queue-model/migrations/20231106142223_add_allow_retry/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `allow_retry` to the `Submission` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `Submission` ADD COLUMN `allow_retry` BOOLEAN NOT NULL;
