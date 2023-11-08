d407461f1da784d8fdbbb1478f71e20b1f7ea62d
Luke Zigler <luke@cautionyourblast.com>
2023-11-08 12:39:57 +0000

    Removed warning from webhook_url migration

--- queue-model/migrations/20231108100812_webhook_url_required/migration.sql
+++ queue-model/migrations/20231108100812_webhook_url_required/migration.sql
@@ -1,10 +1,3 @@
-/*
-  Warnings:
-
-  - Made the column `webhook_url` on table `Submission` required. This step will fail if there are existing NULL values in that column.
-
-*/
-
 -- UpdateColumn
 UPDATE `Submission` SET `webhook_url`='' WHERE `webhook_url` IS NULL;
 
