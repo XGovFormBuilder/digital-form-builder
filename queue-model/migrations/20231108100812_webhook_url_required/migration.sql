0a4364b3b71a81f1b2414866c6cd055249829ac7
Luke Zigler <luke@cautionyourblast.com>
2023-11-08 10:10:02 +0000

    Updated url required migration to set the value of all null webhook_url fields to an empty string before running table modification

--- queue-model/migrations/20231108100812_webhook_url_required/migration.sql
+++ queue-model/migrations/20231108100812_webhook_url_required/migration.sql
@@ -4,5 +4,9 @@
   - Made the column `webhook_url` on table `Submission` required. This step will fail if there are existing NULL values in that column.
 
 */
+
+-- UpdateColumn
+UPDATE `Submission` SET `webhook_url`='' WHERE `webhook_url` IS NULL;
+
 -- AlterTable
 ALTER TABLE `Submission` MODIFY `webhook_url` VARCHAR(191) NOT NULL;
