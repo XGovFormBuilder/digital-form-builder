464ba7824601c36a6444c3477f2262b6edbc717a
Mulberry, Tiernan <tiernan.mulberry@ukhsa.gov.uk>
2023-11-22 13:15:22 +0000

    updating default.js

--- runner/config/default.js
+++ runner/config/default.js
@@ -69,12 +69,14 @@ module.exports = {
   confirmationSessionTimeout: 0,
   paymentSessionTimeout: 90 * minute, // GOV.UK Pay sessions are 90 minutes. It is possible a user takes longer than 20 minutes to complete a payment.
   // - COMMENT OUT TO TEST WITHOUT REDIS -
-   // -----------------------------------------------------
+  // -----------------------------------------------------
+
    sessionCookiePassword: "${SessionCookies.Password}",
    redisHost: "${Redis.Host}",
    redisPort: 6379,
    redisPassword: "${Redis.Password}", // This should be set if you are deploying replicas - SET AS SECRET
    redisTls: true, //run in TLS mode
+
   // ------------------------------------------------------
   /**
    * SSL
