8ff0421f7035c52cfa8d49f61c5f654fbec445cc
Luke Zigler <luke@cautionyourblast.com>
2023-12-14 10:50:26 +0000

    Tidying

--- e2e/cypress/support/step_definitions/runner/i_upload_a_file_that_string.js
+++ e2e/cypress/support/step_definitions/runner/i_upload_a_file_that_string.js
@@ -1,11 +1,6 @@
 import { When } from "@badeball/cypress-cucumber-preprocessor";
 
-When("I upload a file that {string}", (string) => {
-  let file = "bad-file.png";
-  if (string === "passes") {
-    file = "good-file.png";
-  }
-
-  cy.get("input[type=file]").attachFile(file);
+When("I upload a file that {string}", (result) => {
+  cy.get("input[type=file]").attachFile(`${result}.png`);
   cy.findByRole("button", { name: /continue/i }).click();
 });
--- runner/src/server/services/upload/mockUploadService.ts
+++ runner/src/server/services/upload/mockUploadService.ts
@@ -2,28 +2,18 @@ import { UploadService } from "./uploadService";
 
 export class MockUploadService extends UploadService {
   async uploadDocuments(locations: any[]) {
-    const shouldFail =
-      locations.filter((location) => location.hapi.filename === "bad-file.png")
-        .length > 0;
-    const responseData = shouldFail
-      ? {
-          res: {
-            statusCode: 201,
-            headers: {
-              location: "https://document-upload-endpoint",
-            },
-          },
-          payload: "imageQualityWarning",
-        }
-      : {
-          res: {
-            statusCode: 201,
-            headers: {
-              location: "https://document-endpoint.com",
-            },
-          },
-          payload: undefined,
-        };
+    const shouldFailOCR = locations.find(
+      (location) => location.hapi.filename === "fails-ocr.png"
+    );
+    const responseData = {
+      res: {
+        statusCode: 201,
+        headers: {
+          location: "https://document-upload-endpoint",
+        },
+      },
+      payload: shouldFailOCR && "imageQualityWarning",
+    };
 
     return this.parsedDocumentUploadResponse(responseData);
   }
