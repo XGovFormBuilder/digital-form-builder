5c0d324eed8d4748c54a7a2678058dbbf2421c98
Luke Zigler <luke@cautionyourblast.com>
2023-12-14 10:37:27 +0000

    Removed logs

--- runner/src/server/services/upload/mockUploadService.ts
+++ runner/src/server/services/upload/mockUploadService.ts
@@ -2,11 +2,9 @@ import { UploadService } from "./uploadService";
 
 export class MockUploadService extends UploadService {
   async uploadDocuments(locations: any[]) {
-    console.log("The locations: ", locations);
     const shouldFail =
       locations.filter((location) => location.hapi.filename === "bad-file.png")
         .length > 0;
-    console.log("Should fail: ", shouldFail);
     const responseData = shouldFail
       ? {
           res: {
