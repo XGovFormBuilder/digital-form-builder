3bbefd1c06e252e6860b8efa4f0abf25dc490ab1
ziggy-cyb <120458506+ziggy-cyb@users.noreply.github.com>
2024-01-02 17:28:48 +0000

    fix: Updated pay return url to allow insecure redirect urls if the environment is in test (#1178)
    * Updated config schema to allow http if the environment is in test
    
    * Updated pay return url to work in development as well
    
    * Updated config tests
    
    * Added different tests for different scenarios when an insecure return url is allowed
    
    * Added documentation on the pay return url in default.js

--- runner/src/server/utils/configSchema.ts
+++ runner/src/server/utils/configSchema.ts
@@ -31,7 +31,15 @@ export const configSchema = Joi.object({
   matomoId: Joi.string().optional(),
   matomoUrl: Joi.string().custom(secureUrl).optional(),
   payApiUrl: Joi.string().custom(secureUrl),
-  payReturnUrl: Joi.string().custom(secureUrl),
+  payReturnUrl: Joi.when("env", {
+    is: Joi.string().valid("development", "test"),
+    then: Joi.string().default("http://localhost:3009"),
+    otherwise: Joi.when("apiEnv", {
+      is: Joi.string().valid("test"),
+      then: Joi.string().default("http://localhost:3009"),
+      otherwise: Joi.string().custom(secureUrl),
+    }),
+  }),
   serviceUrl: Joi.string().optional(),
   redisHost: Joi.string().optional(),
   redisPort: Joi.number().optional(),
