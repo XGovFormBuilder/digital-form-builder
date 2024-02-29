5d65b49a08d53c1a010c324fca316eaaa4c717f4
Luke Zigler <luke@cautionyourblast.com>
2024-02-29 10:14:19 +0000

    Added a multi page controller

---
+++ runner/src/server/plugins/engine/pageControllers/MultiPageController.ts
@@ -0,0 +1,19 @@
+import { FormData, FormSubmissionErrors } from "../types";
+import { PageController } from "./PageController";
+
+export class MultiPageController extends PageController {
+  get viewName() {
+    return "multi-page";
+  }
+  getViewModel(formData: FormData, errors?: FormSubmissionErrors) {
+    const viewModel = super.getViewModel(formData, errors);
+    const { showContinueButton, startPageNavigation } = this.pageDef;
+    return {
+      ...viewModel,
+      continueButton: showContinueButton && this.pageDef.continueButtonText,
+      prevButton: startPageNavigation.previous,
+      nextButton: startPageNavigation.next,
+      isMultiPageController: true,
+    };
+  }
+}
