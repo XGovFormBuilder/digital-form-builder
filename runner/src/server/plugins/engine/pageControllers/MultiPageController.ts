2020534e157f92da96d335370aed58ba63f76410
Luke Zigler <luke@cautionyourblast.com>
2024-03-01 12:04:53 +0000

    Allowed templating in start page navigation

--- runner/src/server/plugins/engine/pageControllers/MultiPageController.ts
+++ runner/src/server/plugins/engine/pageControllers/MultiPageController.ts
@@ -1,5 +1,7 @@
 import { FormData, FormSubmissionErrors } from "../types";
 import { PageController } from "./PageController";
+import config from "server/config";
+import nunjucks from "nunjucks";
 
 export class MultiPageController extends PageController {
   get viewName() {
@@ -8,6 +10,20 @@ export class MultiPageController extends PageController {
   getViewModel(formData: FormData, errors?: FormSubmissionErrors) {
     const viewModel = super.getViewModel(formData, errors);
     const { showContinueButton, startPageNavigation } = this.pageDef;
+    if (config.allowUserTemplates) {
+      if (startPageNavigation.previous) {
+        startPageNavigation.previous.labelText = nunjucks.renderString(
+          startPageNavigation.previous.labelText,
+          { ...formData }
+        );
+      }
+      if (startPageNavigation.next) {
+        startPageNavigation.next.labelText = nunjucks.renderString(
+          startPageNavigation.next.labelText,
+          { ...formData }
+        );
+      }
+    }
     return {
       ...viewModel,
       continueButtonText: showContinueButton && this.pageDef.continueButtonText,
