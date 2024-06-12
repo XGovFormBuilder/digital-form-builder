0dbb5154f5764cc21c22a7db3222786db683bb0e
emilyjevans <30391244+emilyjevans@users.noreply.github.com>
2024-06-12 12:51:42 +0100

    Revert "remove comment"
    This reverts commit 1fd6dbd9d0359e725dbd4eb236e231e1eb498fbd.

--- runner/src/server/plugins/engine/pageControllers/SummaryPageController.ts
+++ runner/src/server/plugins/engine/pageControllers/SummaryPageController.ts
@@ -81,6 +81,8 @@ export class SummaryPageController extends PageController {
 
       await cacheService.mergeState(request, { progress });
 
+      // viewModel.backLink = progress[progress.length - 2];
+
       if (viewModel.endPage) {
         return redirectTo(
           request,
