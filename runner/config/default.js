8b28ced2616ee4ab60d32c803bfebcac2dee2537
emilyjevans <30391244+emilyjevans@users.noreply.github.com>
2024-07-01 19:06:29 +0100

    uncomment default.js

--- runner/config/default.js
+++ runner/config/default.js
@@ -70,11 +70,11 @@ module.exports = {
   paymentSessionTimeout: 90 * minute, // GOV.UK Pay sessions are 90 minutes. It is possible a user takes longer than 20 minutes to complete a payment.
   // - COMMENT OUT TO TEST WITHOUT REDIS -
   // -----------------------------------------------------
-  // sessionCookiePassword: "${SessionCookies.Password}",
-  // redisHost: "${Redis.Host}",
-  // redisPort: 6379,
-  // redisPassword: "${Redis.Password}", // This should be set if you are deploying replicas - SET AS SECRET
-  // redisTls: true, //run in TLS mode
+  sessionCookiePassword: "${SessionCookies.Password}",
+  redisHost: "${Redis.Host}",
+  redisPort: 6379,
+  redisPassword: "${Redis.Password}", // This should be set if you are deploying replicas - SET AS SECRET
+  redisTls: true, //run in TLS mode
 
   // ------------------------------------------------------
   /**
