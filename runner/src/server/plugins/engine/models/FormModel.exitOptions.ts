3c15940a8d420ab44c495fec28105b521a93621a
jen <jen@cautionyourblast.com>
2024-07-11 11:30:34 +0100

    tidy exit options

--- runner/src/server/plugins/engine/models/FormModel.exitOptions.ts
+++ runner/src/server/plugins/engine/models/FormModel.exitOptions.ts
@@ -1,29 +1,17 @@
 import { FormModel } from "server/plugins/engine/models/FormModel";
-import pino from "pino";
-import config from "server/config";
-const logger = pino().child({ class: "ExitOptions" });
+import { callbackValidation } from "server/plugins/initialiseSession/helpers";
 export class ExitOptions {
   url: string;
   format?: "STATE" | "WEBHOOK";
+
   constructor(exitOptions: FormModel["exitOptions"]) {
-    this.url = ExitOptions.validatedUrl(exitOptions.url, "url");
+    const { value, error } = callbackValidation().validate(exitOptions.url);
+    if (error) {
+      throw new Error(
+        `FormModel.exitOptions initialisation failed, ${exitOptions.url} is not on the safelist`
+      );
+    }
+    this.url = value;
     this.format = exitOptions.format;
-  }
-
-  private static validatedUrl(url: string, propName?: string) {
-    let urlHostname;
-    try {
-      urlHostname = new URL(url).hostname;
-    } catch (err) {
-      logger.error(err, `${propName} (${url}) is not a valid URL`);
-      throw err;
-    }
-
-    if (!config.safelist.includes(urlHostname)) {
-      logger.error(`${propName} (${url}) is not on the allowlist`);
-      throw new Error(`${propName} (${url}) is not on the allowlist`);
-    }
-
-    return url;
   }
 }
