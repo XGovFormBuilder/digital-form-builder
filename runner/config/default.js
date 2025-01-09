71f588cf0f89b5f5c36698c320495c36cc750d51
emilyjevans <30391244+emilyjevans@users.noreply.github.com>
2025-01-09 15:21:12 +0000

    Add sessionCookiePassword to default.js

--- runner/config/default.js
+++ runner/config/default.js
@@ -68,7 +68,7 @@ module.exports = {
   sessionTimeout: 20 * minute,
   confirmationSessionTimeout: 20 * minute,
   paymentSessionTimeout: 90 * minute, // GOV.UK Pay sessions are 90 minutes. It is possible a user takes longer than 20 minutes to complete a payment.
-  // sessionCookiePassword: "",
+  sessionCookiePassword: "${SessionCookies.Password}",
   // redisHost: "http://localhost",
   // redisPort: 6379,
   // redisPassword: nanoid.random(16), // This should be set if you are deploying replicas
