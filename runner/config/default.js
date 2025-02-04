91b5b09c018c43eb2992372956c107d92be61493
emilyjevans <30391244+emilyjevans@users.noreply.github.com>
2025-02-04 15:04:46 +0000

    CAR-486 clear data and return to landing page on DNR pages

--- runner/config/default.js
+++ runner/config/default.js
@@ -56,7 +56,7 @@ module.exports = {
    */
   serviceUrl: "http://localhost:3009", //This is used for redirects back to the runner.
   serviceName: "Webforms",
-  serviceStartPage: "/ReportAnOutbreak/setting",
+  serviceStartPage: "/ReportAnOutbreak/start",
   privacyPolicyUrl: "",
   feedbackLink: "#", // Used in your phase banner. Can be a URL or more commonly mailto mailto:feedback@department.gov.uk
   phaseTag: "beta", // Accepts "alpha" |"beta" | ""
@@ -68,11 +68,11 @@ module.exports = {
   sessionTimeout: 20 * minute,
   confirmationSessionTimeout: 20 * minute,
   paymentSessionTimeout: 90 * minute, // GOV.UK Pay sessions are 90 minutes. It is possible a user takes longer than 20 minutes to complete a payment.
-  sessionCookiePassword: "${SessionCookies.Password}",
-  redisHost: "${Redis.Host}",
-  redisPort: 6379,
-  redisPassword: "${Redis.Password}", // This should be set if you are deploying replicas - SET AS SECRET
-  redisTls: true, //run in TLS mode
+  // sessionCookiePassword: "${SessionCookies.Password}",
+  // redisHost: "${Redis.Host}",
+  // redisPort: 6379,
+  // redisPassword: "${Redis.Password}", // This should be set if you are deploying replicas - SET AS SECRET
+  // redisTls: true, //run in TLS mode
 
   /**
    * SSL
