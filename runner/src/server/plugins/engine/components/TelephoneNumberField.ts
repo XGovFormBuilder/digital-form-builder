b06bd2c6c9a332773491216325e2377c7b1ab6e8
Connor Avery <55276424+connoravo@users.noreply.github.com>
2025-04-25 10:52:03 +0100

    Disallow 04 and 06 numbers in telephone validation regex (#359)
    * CAR-1820 Adjust telephone regex to not allow 04 or 06 numbers
    
    Signed-off-by: Connor Avery <55276424+connoravo@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Connor Avery <55276424+connoravo@users.noreply.github.com>

--- runner/src/server/plugins/engine/components/TelephoneNumberField.ts
+++ runner/src/server/plugins/engine/components/TelephoneNumberField.ts
@@ -7,8 +7,7 @@ import { FormData, FormSubmissionErrors } from "../types";
 import joi, { Schema } from "joi";
 
 const TELEPHONE_REGEX =
-  "^(((\\+44\\s?\\d{4}|\\(?0\\d{4}\\)?)\\s?\\d{3}\\s?\\d{3})|((\\+44\\s?\\d{3}|\\(?0\\d{3}\\)?)\\s?\\d{3}\\s?\\d{4})|((\\+44\\s?\\d{2}|\\(?0\\d{2}\\)?)\\s?\\d{4}\\s?\\d{4}))(\\s?#(\\d{4}|\\d{3}))?$";
-
+  "^(((+44s?(?!4|6)d{4}|(?0(?!4|6)d{4})?)s?d{3}s?d{3})|((+44s?(?!4|6)d{3}|(?0(?!4|6)d{3})?)s?d{3}s?d{4})|((+44s?(?!4|6)d{2}|(?0(?!4|6)d{2})?)s?d{4}s?d{4}))(s?#(d{4}|d{3}))?$";
 export class TelephoneNumberField extends FormComponent {
   constructor(def: TelephoneNumberFieldComponent, model: FormModel) {
     super(def, model);
