02a97a266679de9bb60adbbaaa855480664e2f8d
Connor Avery <55276424+connoravo@users.noreply.github.com>
2025-04-28 11:08:58 +0100

    Fix telephone regex escaping (#364)
    Signed-off-by: Connor Avery <55276424+connoravo@users.noreply.github.com>

--- runner/src/server/plugins/engine/components/TelephoneNumberField.ts
+++ runner/src/server/plugins/engine/components/TelephoneNumberField.ts
@@ -6,8 +6,7 @@ import { addClassOptionIfNone, internationalPhoneValidator } from "./helpers";
 import { FormData, FormSubmissionErrors } from "../types";
 import joi, { Schema } from "joi";
 
-const TELEPHONE_REGEX =
-  "\\^\\(\\(\\(\\+44s\\?\\(\\?!4\\|6\\)d\\{4\\}\\|\\(\\?0\\(\\?!4\\|6\\)d\\{4\\}\\)\\?\\)s\\?d\\{3\\}s\\?d\\{3\\}\\)\\|\\(\\(\\+44s\\?\\(\\?!4\\|6\\)d\\{3\\}\\|\\(\\?0\\(\\?!4\\|6\\)d\\{3\\}\\)\\?\\)s\\?d\\{3\\}s\\?d\\{4\\}\\)\\|\\(\\(\\+44s\\?\\(\\?!4\\|6\\)d\\{2\\}\\|\\(\\?0\\(\\?!4\\|6\\)d\\{2\\}\\)\\?\\)s\\?d\\{4\\}s\\?d\\{4\\}\\)\\)\\(s\\?\\#\\(d\\{4\\}\\|d\\{3\\}\\)\\)\\?\\$";
+const TELEPHONE_REGEX = "^(((\\\+44\\s?(?!4|6)\\d{4}|\\(?0(?!4|6)\\d{4}\\)?)\\s?\\d{3}\\s?\\d{3})|((\\\+44\\s?(?!4|6)\\d{3}|\\(?0(?!4|6)\\d{3}\\)?)\\s?\\d{3}\\s?\\d{4})|((\\\+44\\s?(?!4|6)\\d{2}|\\(?0(?!4|6)\\d{2}\\)?)\\s?\\d{4}\\s?\\d{4}))(\\s?\\#(\\d{4}|\\d{3}))?$";
 export class TelephoneNumberField extends FormComponent {
   constructor(def: TelephoneNumberFieldComponent, model: FormModel) {
     super(def, model);
