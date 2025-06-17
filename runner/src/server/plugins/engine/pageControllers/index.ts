0c617269f8acfe9719415ccd4e3a4e8d56b5f0b1
calum-ukhsa <calum.hemphill@ukhsa.gov.uk>
2025-06-17 13:37:28 +0100

    Update index.ts

--- runner/src/server/plugins/engine/pageControllers/index.ts
+++ runner/src/server/plugins/engine/pageControllers/index.ts
@@ -5,4 +5,5 @@ export { StartDatePageController } from "./StartDatePageController";
 export { StartPageController } from "./StartPageController";
 export { SummaryPageController } from "./SummaryPageController";
 export { PageControllerBase } from "./PageControllerBase";
+export { RepeatingSectionSummaryPageController } from "./RepeatingSectionSummaryPageController";
 export { getPageController, controllerNameFromPath } from "./helpers";
