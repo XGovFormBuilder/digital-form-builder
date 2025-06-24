1a69700e1f69477d158c12e005c6693ebb6790a2
Jen Duong <hi@jen.digital>
2025-06-24 12:48:58 +0100

    add inline documentation

--- runner/src/server/transforms/summaryDetails/index.ts
+++ runner/src/server/transforms/summaryDetails/index.ts
@@ -3,6 +3,9 @@
 import { SummaryDetailsTransformationMap } from "server/transforms/summaryDetails/types";
 export { SummaryDetailsTransformationMap };
 
+/**
+ * [View the docs for summary-details-transformations an explanation of how this feature works](docs/runner/summary-details-transforms.md)
+ */
 const summaryDetailsTransformations: SummaryDetailsTransformationMap = {};
 
 module.exports = summaryDetailsTransformations;
