d3cab1ae263b3e2cba11a5962416a74442711460
calum-ukhsa <calum.hemphill@ukhsa.gov.uk>
2025-06-27 15:01:53 +0100

    Add closeContact to summaryDetails/index.ts

--- runner/src/server/transforms/summaryDetails/index.ts
+++ runner/src/server/transforms/summaryDetails/index.ts
@@ -6,6 +6,8 @@ export { SummaryDetailsTransformationMap };
 /**
  * [View the docs for summary-details-transformations an explanation of how this feature works](docs/runner/summary-details-transforms.md)
  */
-const summaryDetailsTransformations: SummaryDetailsTransformationMap = {};
+const summaryDetailsTransformations: SummaryDetailsTransformationMap = {
+  "close-contact-form": closeContact,
+};
 
 module.exports = summaryDetailsTransformations;
