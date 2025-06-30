dab7a0928c5ca6276dca3606666710b1798a93e4
calum-ukhsa <calum.hemphill@ukhsa.gov.uk>
2025-06-30 16:30:43 +0100

    Update index.ts

--- runner/src/server/transforms/summaryDetails/index.ts
+++ runner/src/server/transforms/summaryDetails/index.ts
@@ -1,11 +1,15 @@
 "use strict";
 
+import { sectionsOnlyAndCardConversion } from "./sectionsOnlyAndCardConversion";
+
 import { SummaryDetailsTransformationMap } from "server/transforms/summaryDetails/types";
 export { SummaryDetailsTransformationMap };
 
 /**
  * [View the docs for summary-details-transformations an explanation of how this feature works](docs/runner/summary-details-transforms.md)
  */
-const summaryDetailsTransformations: SummaryDetailsTransformationMap = {};
+const summaryDetailsTransformations: SummaryDetailsTransformationMap = {
+  "close-contact-form": sectionsOnlyAndCardConversion,
+};
 
 module.exports = summaryDetailsTransformations;
