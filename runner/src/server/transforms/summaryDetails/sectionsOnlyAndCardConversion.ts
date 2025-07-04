75f6a0bbf34977562e874373d8a4b386729cbf9e
calum-ukhsa <calum.hemphill@ukhsa.gov.uk>
2025-07-04 15:36:03 +0100

    Update sectionsOnlyAndCardConversion.ts

--- runner/src/server/transforms/summaryDetails/sectionsOnlyAndCardConversion.ts
+++ runner/src/server/transforms/summaryDetails/sectionsOnlyAndCardConversion.ts
@@ -1,9 +1,12 @@
-export function sectionsOnlyAndCardConversion(details) {
+export function filterSections(details) {
   return details
     .filter((detail) => detail.name)
     .map((detail) => {
-      const { url } = detail.items[0];
-      if (detail.name.match(/\w\d/)) return { ...detail, card: url };
+      if (detail.name.match(/\w\d/)) {
+        detail.card = detail.items.find((item) => item.inError)
+          ? detail.items[0].pageId
+          : detail.items[0].url;
+      }
       return detail;
     });
 }
