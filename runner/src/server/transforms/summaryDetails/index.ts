45d1c7bcf01d319776454e20ad8538483b160b47
calum-ukhsa <calum.hemphill@ukhsa.gov.uk>
2025-07-18 11:36:11 +0100

    Update index.ts

--- runner/src/server/transforms/summaryDetails/index.ts
+++ runner/src/server/transforms/summaryDetails/index.ts
@@ -27,18 +27,22 @@ const closeContactParams = [
     joiner: " ",
   },
   {
-    names: ["contact_phone_number", "contact_email_address"],
+    names: ["contact_phone_number", "contact_email"],
     to: "Contact details",
     joiner: "\n",
   },
 ];
 
 const summaryDetailsTransformations: SummaryDetailsTransformationMap = {
-  "close-contact-form": (details) => {
+  "close-contact-form-nl1-dev": (details) => {
     const firstTransform = mergeRows(details, closeContactParams);
     return filterSections(firstTransform);
   },
-  "close-contact-form-uat": (details) => {
+  "close-contact-form-nl1-test": (details) => {
+    const firstTransform = mergeRows(details, closeContactParams);
+    return filterSections(firstTransform);
+  },
+  "close-contact-form-nl4": (details) => {
     const firstTransform = mergeRows(details, closeContactParams);
     return filterSections(firstTransform);
   },
@@ -46,15 +50,43 @@ const summaryDetailsTransformations: SummaryDetailsTransformationMap = {
     const firstTransform = mergeRows(details, closeContactParams);
     return filterSections(firstTransform);
   },
-  "close-contact-form-hpt": (details) => {
+  "close-contact-form-nl7": (details) => {
     const firstTransform = mergeRows(details, closeContactParams);
     return filterSections(firstTransform);
   },
-  "close-contact-form-hpt-uat": (details) => {
+  "close-contact-form-nl8": (details) => {
+    const firstTransform = mergeRows(details, closeContactParams);
+    return filterSections(firstTransform);
+  },
+  "close-contact-form-uat": (details) => {
+    const firstTransform = mergeRows(details, closeContactParams);
+    return filterSections(firstTransform);
+  },
+  "close-contact-form-hpt-nl1-dev": (details) => {
+    const firstTransform = mergeRows(details, closeContactParams);
+    return filterSections(firstTransform);
+  },
+  "close-contact-form-hpt-nl1-test": (details) => {
+    const firstTransform = mergeRows(details, closeContactParams);
+    return filterSections(firstTransform);
+  },
+  "close-contact-form-hpt-nl4": (details) => {
     const firstTransform = mergeRows(details, closeContactParams);
     return filterSections(firstTransform);
   },
   "close-contact-form-hpt-nl5": (details) => {
+    const firstTransform = mergeRows(details, closeContactParams);
+    return filterSections(firstTransform);
+  },
+  "close-contact-form-hpt-nl7": (details) => {
+    const firstTransform = mergeRows(details, closeContactParams);
+    return filterSections(firstTransform);
+  },
+  "close-contact-form-hpt-nl8": (details) => {
+    const firstTransform = mergeRows(details, closeContactParams);
+    return filterSections(firstTransform);
+  },
+  "close-contact-form-hpt-uat": (details) => {
     const firstTransform = mergeRows(details, closeContactParams);
     return filterSections(firstTransform);
   },
