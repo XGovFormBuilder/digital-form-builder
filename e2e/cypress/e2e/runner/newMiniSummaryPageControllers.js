00663f47f062d198f7032f6545176fd7596f17df
calum-ukhsa <calum.hemphill@ukhsa.gov.uk>
2025-07-29 15:42:59 +0100

    Create newMiniSummaryPageControllers.js

---
+++ e2e/cypress/e2e/runner/newMiniSummaryPageControllers.js
@@ -0,0 +1,7 @@
+Given("a page utilises the MiniSummaryPageController", function () {
+  return "pending";
+});
+
+Given("a page utilises the RepeatingSectionSummaryPageController", function () {
+  return "pending";
+});
