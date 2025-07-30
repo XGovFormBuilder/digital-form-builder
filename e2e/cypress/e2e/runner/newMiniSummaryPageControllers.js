be63637120e2a07529d9ad9fdb9a64a75a4283c6
calum-ukhsa <calum.hemphill@ukhsa.gov.uk>
2025-07-30 15:30:17 +0100

    Delete e2e/cypress/e2e/runner/newMiniSummaryPageControllers.js

--- e2e/cypress/e2e/runner/newMiniSummaryPageControllers.js
+++
@@ -1,9 +0,0 @@
-import { Given } from "@badeball/cypress-cucumber-preprocessor";
-
-Given("a page utilises the MiniSummaryPageController", function () {
-  return "pending";
-});
-
-Given("a page utilises the RepeatingSectionSummaryPageController", function () {
-  return "pending";
-});
