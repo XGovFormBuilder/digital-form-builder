4d69dd69511bc93f5b2a59f72e701c04887524e8
kitttang <katrina.tang@accenture.com>
2025-10-14 13:38:23 +0100

    remove js, put back feature

--- e2e/cypress/e2e/runner/confirmationTimeout.js
+++
@@ -1,6 +0,0 @@
-import { When } from "@badeball/cypress-cucumber-preprocessor";
-
-When("I upload the file {string}", (filename) => {
-  cy.get("input[type=file]").attachFile(filename);
-  cy.findByRole("button", { name: /continue/i }).click();
-});
