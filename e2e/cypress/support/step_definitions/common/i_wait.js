fbb1ac4aa677f75cfb15010ec5a8fc33f8688f86
kitttang <katrina.tang@accenture.com>
2025-10-14 13:26:25 +0100

    Update i_wait.js

--- e2e/cypress/support/step_definitions/common/i_wait.js
+++ e2e/cypress/support/step_definitions/common/i_wait.js
@@ -1,7 +1,5 @@
 import { When } from "@badeball/cypress-cucumber-preprocessor";
 
-// Accept either:  When I wait 1000 milliseconds
-// or: When I wait "1000" milliseconds
 When("I wait {int} milliseconds", (milliseconds) => {
   cy.wait(Number(milliseconds));
 });
